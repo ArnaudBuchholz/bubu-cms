c577643431c0c14c49f69eda77ef946669fb83dd
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-03 00:32:06 -0400

    loads gpf-js

--- public/js/loader.js
+++ public/js/loader.js
@@ -4,7 +4,11 @@
     document.body.innerHTML = "<table width=\"100%\" height=\"100%\"><tr valign=\"center\"><td align=\"center\">"
         + "<progress id=\"progress\"></progress></td></tr></table>";
     var progress = document.getElementById("progress");
-    progress.value = 1;
+    progress.value = 0;
     progress.max = 10;
+
+    var script = document.createElement("script");
+    script.src = "js/gpf.js";
+    document.body.appendChild(script);
 
 }());
