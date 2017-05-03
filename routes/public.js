a78e273e435e228c812a7eb8a3e749c553b7e56c
Arnaud Buchholz <arnaud.buchholz@free.fr>
2017-05-03 00:32:20 -0400

    loads gpf-js

---
+++ routes/public.js
@@ -0,0 +1,11 @@
+const
+    express = require("express"),
+    router = express.Router(),
+    path = require("path");
+
+/* GET home page. */
+router.get("/js/gpf.js", (req, res, next) => {
+    res.sendFile(path.join(__dirname + "/../node_modules/gpf-js/build/gpf.js"));
+});
+
+module.exports = router;
