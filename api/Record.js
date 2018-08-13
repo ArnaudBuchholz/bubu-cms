f477c2ea8d3d55297e3b92fad5d5b15bc93d5582
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-13 16:32:11 -0400

    Record class with attributes

--- api/Record.js
+++ api/Record.js
@@ -1,12 +1,15 @@
 "use strict";
 
+const
+    attributes = require("./attributes.js");
+
 gpf.define({
     $class: "Record"
 
-    constructor: function () {
-    },
+    "[_id]": [new attributes.Hidden(), new attributes.Length(10)],
+    _id: "",
 
-    "[_id]": [],
-    _id: ""
+    "[_name]": [new attributes.Length(128), new attributes.Sortable(), new attributes.Filterable()],
+    _name: ""
 
 });
