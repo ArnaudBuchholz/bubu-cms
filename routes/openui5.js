b13bcbb2c421adcdd7578e13b6e6de5940ad5204
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-08-17 12:49:30 -0400

    Handles -dbg.js path

--- routes/openui5.js
+++ routes/openui5.js
@@ -4,18 +4,27 @@ const
     router = express.Router(),
     path = require("path"),
     fs = require("fs"),
-    basePath = path.join(__dirname, "/../bower_components");
+    basePath = path.join(__dirname, "/../bower_components"),
+    trimDbg = url => {
+        if (url.endsWith("sap-ui-core-dbg.js")) {
+            return url;
+        }
+        if (url.endsWith("-dbg.js")) {
+            return url.substr(0, url.length - 7) + ".js";
+        }
+        return url;
+    };
 
 router.get(/.*\.(css|woff2?)$/, (req, res, next) => {
     res.sendFile(path.join(basePath, "openui5-themelib_sap_belize", "resources", req.url));
 });
 
 router.get(/sap\/m\/.*/, (req, res, next) => {
-    res.sendFile(path.join(basePath, "openui5-sap.m", "resources", req.url));
+    res.sendFile(path.join(basePath, "openui5-sap.m", "resources", trimDbg(req.url)));
 });
 
 router.get(/.*/, (req, res, next) => {
-    res.sendFile(path.join(basePath, "openui5-sap.ui.core", "resources", req.url));
+    res.sendFile(path.join(basePath, "openui5-sap.ui.core", "resources", trimDbg(req.url)));
 });
 
 module.exports = router;
