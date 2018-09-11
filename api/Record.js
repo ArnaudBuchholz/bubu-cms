aff89ab5ff74af5e8447728966e20b30ff8e3c69
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-11 17:04:41 -0400

    Flag Record

--- api/Record.js
+++ api/Record.js
@@ -1,15 +1,28 @@
 "use strict";
 
 const
-    attributes = require("./attributes.js");
+    attributes = require("./attributes.js"),
+    key = new attributes.Key(),
+    sortable = new attributes.Sortable(),
+    filterable = new attributes.Filterable(),
+    updatable = new attributes.Updatable(),
+    creatable = new attributes.Creatable();
 
 module.exports = gpf.define({
     $class: "Record",
 
-    "[_id]": [new attributes.Hidden(), new attributes.Length(10)],
+    "[_id]": [key, new gpf.attributes.Serializable({
+        name: "id",
+        type: gpf.serial.types.string,
+        required: true
+    })],
     _id: "",
 
-    "[_name]": [new attributes.Length(128), new attributes.Sortable(), new attributes.Filterable()],
+    "[_name]": [creatable, updatable, sortable, filterable, new gpf.attributes.Serializable({
+        name: "name",
+        type: gpf.serial.types.string,
+        required: true
+    })],
     _name: ""
 
 });
