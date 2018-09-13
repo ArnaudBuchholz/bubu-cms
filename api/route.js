3e2e9dbf2354bfd7c556c11f7b8f7549da26b639
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-12 23:46:23 -0400

    Starting to implement GET

--- api/route.js
+++ api/route.js
@@ -4,6 +4,8 @@ const
     router = express.Router(),
 
     metadata = require("./metadata"),
+    entities = require("./entities"),
+    db = require("./db"),
 
     notImplemented = next => {
         var error = new Error("Not implemented");
@@ -15,6 +17,15 @@ router.get(/\/\$metadata.*/, (req, res, next) => {
     res.set("Content-Type", "application/xml");
     metadata().then(body => res.send(body))
 });
+
+entities.forEach(EntityClass => {
+    router.get(new RegExp(`\/${EntityClass.name}Set\?.*`), (req, res, next) =>
+        db.open().then(() => {
+            res.set("Content-Type", "application/json");
+            res.send(JSON.stringify(EntityClass.get()));
+        }));
+});
+
 router.get(/.*/, (req, res, next) => notImplemented(next));
 
 module.exports = router;
