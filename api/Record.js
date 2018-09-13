a26ddd6631ecce9d21a68e2405c09dae88afab8e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-12 23:45:42 -0400

    Adds static methods

--- api/Record.js
+++ api/Record.js
@@ -6,23 +6,35 @@ const
     sortable = new attributes.Sortable(),
     filterable = new attributes.Filterable(),
     updatable = new attributes.Updatable(),
-    creatable = new attributes.Creatable();
+    creatable = new attributes.Creatable(),
 
-module.exports = gpf.define({
-    $class: "Record",
+    Record = gpf.define({
+        $class: "Record",
 
-    "[_id]": [key, new gpf.attributes.Serializable({
-        name: "id",
-        type: gpf.serial.types.string,
-        required: true
-    })],
-    _id: "",
+        "[_id]": [key, new gpf.attributes.Serializable({
+            name: "id",
+            type: gpf.serial.types.string,
+            required: true
+        })],
+        _id: "",
 
-    "[_name]": [creatable, updatable, sortable, filterable, new gpf.attributes.Serializable({
-        name: "name",
-        type: gpf.serial.types.string,
-        required: true
-    })],
-    _name: ""
+        "[_name]": [creatable, updatable, sortable, filterable, new gpf.attributes.Serializable({
+            name: "name",
+            type: gpf.serial.types.string,
+            required: true
+        })],
+        _name: ""
+
+    }),
+
+    records = [];
+
+Object.assign(Record, {
+
+    get: () => records,
+
+    load: array => Promise.resolve([].splice.apply(records, [0,0].concat(array)))
 
 });
+
+module.exports = Record;
