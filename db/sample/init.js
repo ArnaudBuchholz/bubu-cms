99a3f0506ed31ad5061421eb74483fd50821168e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-17 12:48:45 -0400

    Generate sample attributes

--- db/sample/init.js
+++ db/sample/init.js
@@ -12,6 +12,19 @@ module.exports = db => {
         const record = new Record();
         record._id = nanoid();
         record._name = `Generated record ${idx}`;
+        if (idx) {
+            record._icon = "sap-icon://business-one";
+            record._number = "" + idx;
+            record._numberUnit = "EACH";
+        } else {
+            record._icon = "/images/recipe.jpg";
+            record._number = "320";
+            record._numberUnit = "kCal/portion";
+        }
+        const rating = idx % 6;
+        if (rating) {
+            record._rating = "" + rating;
+        }
         records.push(record);
     }
     return db.loadRecords(records);
