6d0db74c9ae61e2d1024e1a2a1841a38f0f0e37d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-18 13:04:53 -0400

    Use CSV reader

--- db/sample/init.js
+++ db/sample/init.js
@@ -1,31 +1,28 @@
 "use strict";
 
 const
-    COUNT = 100,
+    gpf = require("gpf-js/source"),
     nanoid = require("nanoid"),
     Record = require("../../api/Record"),
-    records = [];
+    path = require("path");
 
 module.exports = db => {
-    console.log("Generating sample database...");
-    for (let idx = 0; idx < COUNT; ++idx) {
-        const record = new Record();
-        record._id = nanoid();
-        record._name = `Generated record ${idx}`;
-        if (idx) {
-            record._icon = "sap-icon://business-one";
-            record._number = "" + idx;
-            record._numberUnit = "EACH";
-        } else {
-            record._icon = "/images/recipe.jpg";
-            record._number = "320";
-            record._numberUnit = "kCal/portion";
-        }
-        const rating = idx % 6;
-        if (rating) {
-            record._rating = "" + rating;
-        }
-        records.push(record);
-    }
-    return db.loadRecords(records);
+    const
+        csvFilePromise = gpf.fs.getFileStorage()
+            .openTextStream(path.join(__dirname, "records.csv"), gpf.fs.openFor.reading),
+        lineAdapter = new gpf.stream.LineAdapter(),
+        csvParser = new gpf.stream.csv.Parser(),
+        deserialize = gpf.serial.buildFromRaw(Record),
+        map = new gpf.stream.Map(function (raw) {
+            return deserialize(new Record(), Object.assign({
+                id: nanoid()
+            }, raw));
+        }),
+        output = new gpf.stream.WritableArray();
+
+    return csvFilePromise
+        .then(csvFile => gpf.stream.pipe(csvFile, lineAdapter, csvParser, map, output)
+            .then(function () {
+                return db.loadRecords(output.toArray());
+            }));
 };
