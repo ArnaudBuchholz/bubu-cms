b40ff88161b608991595b8be6115bceb4431ade5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-22 09:41:11 -0400

    Rating renderer

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,6 +9,11 @@ sap.ui.define([
 
 	return Controller.extend("bubu-cms.controller.App", {
 
+		renderRating: function (value) {
+			return new Array(value + 1).join("\u2605")
+				+ new Array(6 - value).join("\u2606");
+		},
+
 		onInit: function() {
 			var oI18nResourceBundle = this.getView().getModel("i18n").getResourceBundle();
 			this._i18n = oI18nResourceBundle.getText.bind(oI18nResourceBundle);
