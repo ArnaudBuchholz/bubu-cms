da5cb718875b68d2c72c16d1a6eba22557fbb6ce
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-24 13:19:41 -0400

    Split the data into several files

--- db/sample/init.js
+++ db/sample/init.js
@@ -4,29 +4,28 @@ const
     gpf = require("gpf-js/source"),
     nanoid = require("nanoid"),
     Record = require("../../api/Record"),
-    path = require("path");
+    path = require("path"),
 
-module.exports = db => {
-    const
-        csvFilePromise = gpf.fs.getFileStorage()
-            .openTextStream(path.join(__dirname, "records.csv"), gpf.fs.openFor.reading),
-        lineAdapter = new gpf.stream.LineAdapter(),
-        csvParser = new gpf.stream.csv.Parser(),
-        map = new gpf.stream.Map(function (raw) {
-            return gpf.serial.fromRaw(new Record(), Object.assign({
-                id: nanoid()
-            }, raw), function (value, property) {
-                if (property.type === gpf.serial.types.integer) {
-                    return parseInt(value, 10);
-                }
-                return value;
-            });
-        }),
-        output = new gpf.stream.WritableArray();
-
-    return csvFilePromise
-        .then(csvFile => gpf.stream.pipe(csvFile, lineAdapter, csvParser, map, output)
-            .then(function () {
-                return db.loadRecords(output.toArray());
-            }));
-};
+    _loadCSV = (db, name, RecordType) => {
+        const
+            csvFilePromise = gpf.fs.getFileStorage()
+                .openTextStream(path.join(__dirname, `${name}.csv`), gpf.fs.openFor.reading),
+            lineAdapter = new gpf.stream.LineAdapter(),
+            csvParser = new gpf.stream.csv.Parser(),
+            map = new gpf.stream.Map(function (raw) {
+                return new RecordType(Object.assign({
+                    id: nanoid() // as of now
+                }, raw));
+            }),
+            output = new gpf.stream.WritableArray();
+        return csvFilePromise
+            .then(csvFile => gpf.stream.pipe(csvFile, lineAdapter, csvParser, map, output)
+                .then(function () {
+                    return db.loadRecords(output.toArray());
+                }));
+    }
+;
+debugger;
+module.exports = db => Promise.all([
+    _loadCSV(db, "recipe", require("./Recipe")(db))
+]);
