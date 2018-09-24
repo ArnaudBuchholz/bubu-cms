90d6afb8f380b47e8b4f0117da8ff4223c5a5286
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-24 13:18:55 -0400

    Generic status attributes

--- api/Record.js
+++ api/Record.js
@@ -60,12 +60,60 @@ const
             name: "modified",
             type: gpf.serial.types.datetime
         })],
-        _modified: null
+        _modified: null,
+
+        "[_statusTitle1]": [new gpf.attributes.Serializable({
+            name: "statusTitle1",
+            type: gpf.serial.types.string
+        })],
+        _statusTitle1: "",
+
+        "[_statusText1]": [creatable, sortable, filterable, new gpf.attributes.Serializable({
+            name: "statusText1",
+            type: gpf.serial.types.string
+        })],
+        _statusText1: "",
+
+        "[_statusState1]": [new gpf.attributes.Serializable({
+            name: "statusState1",
+            type: gpf.serial.types.string
+        })],
+        _statusState1: "None",
+
+        "[_statusTitle2]": [new gpf.attributes.Serializable({
+            name: "statusTitle2",
+            type: gpf.serial.types.string
+        })],
+        _statusTitle2: "",
+
+        "[_statusText2]": [creatable, sortable, filterable, new gpf.attributes.Serializable({
+            name: "statusText2",
+            type: gpf.serial.types.string
+        })],
+        _statusText2: "",
+
+        "[_statusState2]": [new gpf.attributes.Serializable({
+            name: "statusState2",
+            type: gpf.serial.types.string
+        })],
+        _statusState2: "None",
+
+        constructor: function (raw) {
+            gpf.serial.fromRaw(this, raw);
+        }
+
     }),
 
     records = [];
 
 Object.assign(Record, {
+
+    STATE: {
+        error: "Error",
+        none: "None",
+        success: "Success",
+        warning: "Warning"
+    },
 
     get: () => records,
 
