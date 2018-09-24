9403841fa783ea326223dff4a9488b310e73a34d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-24 13:18:44 -0400

    Specializes sub class

---
+++ db/sample/Recipe.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = db => gpf.define({
+    $class: "Recipe",
+    $extend: db.Record,
+
+    _numberUnit: "kCal per portion",
+    _statusTitle1: "portions",
+
+    constructor: function (raw) {
+        const
+            calories = parseInt(raw.calories, 10),
+            portions = parseInt(raw.portions, 10);
+        this._number = Math.floor(calories / portions);
+        this._statusText1 = portions;
+        raw.rating = parseInt(raw.rating, 10);
+        this.$super(raw);
+    }
+});
