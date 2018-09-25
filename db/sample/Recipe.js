0dc413f2263a94ed5e0b17a5731aa54fbdc78bf2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-25 19:45:01 -0400

    Enabling statusText1

--- db/sample/Recipe.js
+++ db/sample/Recipe.js
@@ -4,13 +4,14 @@ module.exports = db => gpf.define({
     $class: "Recipe",
     $extend: db.Record,
 
+    _statusState1: db.Record.STATE.show,
+
     constructor: function (raw) {
         const
             calories = parseInt(raw.calories, 10),
             portions = parseInt(raw.portions, 10);
         this._number = Math.floor(calories / portions);
         this._statusText1 = portions;
-        this._statusState1 = db.Record.STATE.show;
         this.$super(raw);
     }
 });
