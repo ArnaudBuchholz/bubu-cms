cc4bd9b429560a0f1ab2a144484b64e40da5a925
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-25 19:19:11 -0400

    Adapts to new format

--- db/sample/Recipe.js
+++ db/sample/Recipe.js
@@ -4,15 +4,13 @@ module.exports = db => gpf.define({
     $class: "Recipe",
     $extend: db.Record,
 
-    _numberUnit: "kCal per portion",
-    _statusTitle1: "portions",
-
     constructor: function (raw) {
         const
             calories = parseInt(raw.calories, 10),
             portions = parseInt(raw.portions, 10);
         this._number = Math.floor(calories / portions);
         this._statusText1 = portions;
+        this._statusState1 = db.Record.STATE.show;
         this.$super(raw);
     }
 });
