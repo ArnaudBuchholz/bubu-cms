50342a6b508fd4f609d4a9de9204a78c594ad3cc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-25 19:20:41 -0400

    Translate 'static' typed properties

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,6 +9,25 @@ sap.ui.define([
 
 	return Controller.extend("bubu-cms.controller.App", {
 
+		dbI18n: function (key, params) {
+			if (!this._dbI18n) {
+				this._dbI18n = this.getView().getModel("db.i18n").getResourceBundle();
+			}
+			return this._dbI18n.getText(key, params);
+		},
+
+		formatNumberUnit: function (type) {
+			return this.dbI18n(type + ".numberUnit");
+		},
+
+		formatStatus1: function (type) {
+			return this.dbI18n(type + ".status1");
+		},
+
+		formatStatus2: function (type) {
+			return this.dbI18n(type + ".status2");
+		},
+
 		renderRating: function (value) {
 			return new Array(value + 1).join("\u2605")
 				+ new Array(6 - value).join("\u2606");
