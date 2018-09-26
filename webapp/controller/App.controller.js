ef3b6c65f1863022248884a0784d025883ba3f31
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-26 13:12:21 -0400

    Implements an override mechanism in translation: db has priority

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,18 +9,40 @@ sap.ui.define([
 
 	return Controller.extend("bubu-cms.controller.App", {
 
+		getTextIfInI18n: function (resourceBundle, key, params) {
+			if (resourceBundle.hasText(key)) {
+				return resourceBundle.getText(key, params);
+			}
+		},
+
 		dbI18n: function (key, params) {
 			if (!this._dbI18n) {
 				this._dbI18n = this.getView().getModel("db.i18n").getResourceBundle();
 			}
-			return this._dbI18n.getText(key, params);
+			return this.getTextIfInI18n(this._dbI18n, key, params);
+		},
+
+		uiI18n: function (key, params) {
+			if (!this._uiI18n) {
+				this._uiI18n = this.getView().getModel("i18n").getResourceBundle();
+			}
+			return this.getTextIfInI18n(this._uiI18n, key, params);
+		},
+
+		i18n: function (type, key, parameters) {
+			var translationKey = type + "." + key,
+				result = this.dbI18n(translationKey, parameters);
+			if (undefined === result) {
+				result = this.uiI18n(translationKey, parameters);
+			}
+			return result;
 		},
 
 		formatIcon: function (type, icon) {
 			if (icon) {
 				return icon;
 			}
-			var defaultIcon = this.dbI18n(type + ".defaultIcon");
+			var defaultIcon = this.i18n(type, "defaultIcon");
 			if (defaultIcon) {
 				return "sap-icon://" + defaultIcon;
 			}
@@ -28,15 +50,15 @@ sap.ui.define([
 		},
 
 		formatNumberUnit: function (type) {
-			return this.dbI18n(type + ".numberUnit");
+			return this.i18n(type, "numberUnit");
 		},
 
 		formatStatus1: function (type) {
-			return this.dbI18n(type + ".status1");
+			return this.i18n(type, "status1");
 		},
 
 		formatStatus2: function (type) {
-			return this.dbI18n(type + ".status2");
+			return this.i18n(type, "status2");
 		},
 
 		renderRating: function (value) {
