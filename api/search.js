276d03b26c37e898dd3bb5b29356bb9a42de04ea
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-09-28 00:01:48 -0400

    Search helper

---
+++ api/search.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const
+    Tag = require("./Tag")
+;
+
+module.exports = (EntityClass, search) => {
+    const
+        searchTerms = search
+            .split(" ")
+            .map(term => term.trim())
+            .filter(term => term),
+        tags = searchTerms
+            .filter(term => term.charAt(0) === "#")
+            .map(term => Tag.get(term.substr(1)))
+            .filter(tag => tag)
+            .sort((a, b) => a._records.length - b._records.length), // Less references first
+        terms = searchTerms
+            .filter(term => term.charAt(0) !== "#")
+    ;
+    let
+        records;
+    if (tags.length) {
+        // AND
+        records = tags[0].records(EntityClass);
+        tags.slice(1).forEach(tag => {
+            records = records.filter(record => record.hasTag(tag));
+        });
+    } else {
+        records = EntityClass.all();
+    }
+    if (terms.length) {
+        // OR
+        records = records.filter(record => terms.some(term => record.search(term)));
+    }
+    return records;
+}
