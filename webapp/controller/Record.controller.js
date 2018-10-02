d0db00a6bf1c67464456c84d22235a9e5c8969ee
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-02 13:23:38 -0400

    Bind the requested record

--- webapp/controller/Record.controller.js
+++ webapp/controller/Record.controller.js
@@ -6,6 +6,41 @@ sap.ui.define([
 
 	return Controller.extend("bubu-cms.controller.Record", {
 
+		_getRouter: function () {
+			return this.getOwnerComponent().getRouter();
+		},
+
+		onInit: function () {
+			this._getRouter().getRoute("record").attachPatternMatched(this._onDisplayRecord, this);
+		},
+
+		_onDisplayRecord: function (event) {
+			var recordId = event.getParameter("arguments").recordId,
+				sPath = "/" + this.getOwnerComponent().getModel().createKey("RecordSet", {
+					id: recordId
+				}),
+				recordPage = this.byId("recordPage");
+			this.getView().bindElement({
+				path: sPath,
+				events: {
+					change: this._onBindingChanged.bind(this),
+					dataRequested: function () {
+						recordPage.setBusy(true);
+					},
+					dataReceived: function () {
+						recordPage.setBusy(false);
+					}
+				}
+			});
+		},
+
+		_onBindingChanged: function () {
+			var binding = this.getView().getElementBinding();
+			if (!binding.getBoundContext()) {
+				alert("problem");
+			}
+		}
+
 	});
 
 });
