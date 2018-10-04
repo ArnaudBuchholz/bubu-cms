c78634bfa6c404c325f62e4856ca23c1d58bc035
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-04 13:33:45 -0400

    Isolates common formatters

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,18 +1,14 @@
 sap.ui.define([
+	"./BaseController",
 	"sap/m/MenuItem",
-	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/routing/HashChanger",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/Sorter",
 
-], function(MenuItem, Controller, HashChanger, JSONModel, Sorter) {
+], function(BaseController, MenuItem, HashChanger, JSONModel, Sorter) {
 	"use strict";
 
-	return Controller.extend("bubu-cms.controller.List", {
-
-		_getRouter: function () {
-			return this.getOwnerComponent().getRouter();
-		},
+	return BaseController.extend("bubu-cms.controller.List", {
 
 		_buildSortingMenu: function () {
 			var sortMenu = this.byId("sortMenu"),
@@ -88,63 +84,6 @@ sap.ui.define([
 			});
 			binding.sort(new Sorter(sortParts[1], sortParts[2] === "Desc"));
 			binding.refresh();
-		},
-
-		getTextIfInI18n: function (resourceBundle, key, params) {
-			if (resourceBundle.hasText(key)) {
-				return resourceBundle.getText(key, params);
-			}
-		},
-
-		dbI18n: function (key, params) {
-			if (!this._dbI18n) {
-				this._dbI18n = this.getView().getModel("db.i18n").getResourceBundle();
-			}
-			return this.getTextIfInI18n(this._dbI18n, key, params);
-		},
-
-		uiI18n: function (key, params) {
-			if (!this._uiI18n) {
-				this._uiI18n = this.getView().getModel("i18n").getResourceBundle();
-			}
-			return this.getTextIfInI18n(this._uiI18n, key, params);
-		},
-
-		i18n: function (type, key, parameters) {
-			var translationKey = type + "." + key,
-				result = this.dbI18n(translationKey, parameters);
-			if (undefined === result) {
-				result = this.uiI18n(translationKey, parameters);
-			}
-			return result;
-		},
-
-		formatIcon: function (type, icon) {
-			if (icon) {
-				return icon;
-			}
-			var defaultIcon = this.i18n(type, "defaultIcon");
-			if (defaultIcon) {
-				return "sap-icon://" + defaultIcon;
-			}
-			return "";
-		},
-
-		formatNumberUnit: function (type) {
-			return this.i18n(type, "numberUnit");
-		},
-
-		formatStatus1: function (type) {
-			return this.i18n(type, "status1");
-		},
-
-		formatStatus2: function (type) {
-			return this.i18n(type, "status2");
-		},
-
-		renderRating: function (value) {
-			return new Array(value + 1).join("\u2605")
-				+ new Array(6 - value).join("\u2606");
 		},
 
 		_setQueryParameter: function (name, value) {
