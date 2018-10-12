ff81470027bd9fb54ccd58b73b2edcf0cf35cbc0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-12 13:33:07 -0400

    Simplifies page

--- webapp/controller/Record.controller.js
+++ webapp/controller/Record.controller.js
@@ -13,24 +13,6 @@ sap.ui.define([
 
 		onInit: function () {
 			this._getRouter().getRoute("record").attachPatternMatched(this._onDisplayRecord, this);
-			this.getView().setModel(new JSONModel({
-				options: [{
-					value: 0
-				}, {
-					value: 1
-				}, {
-					value: 2
-				}, {
-					value: 3
-				}, {
-					value: 4
-				}, {
-					value: 5
-				}]
-			}), "rating");
-			this.getView().setModel(new JSONModel({
-				count: 0
-			}), "tags");
 		},
 
 		_onDisplayRecord: function (event) {
@@ -53,14 +35,20 @@ sap.ui.define([
 		_onBindingChanged: function () {
 			var page = this.byId("page"),
 				binding = this.getView().getElementBinding(),
-				tokens = this.byId("tokens"),
 				record;
 			if (!binding.getBoundContext()) {
 				alert("problem");
 			}
 			record = binding.getBoundContext().getObject();
-			this.getView().getModel("tags").setProperty("/count", record.tags.split(" ").length);
 			this.byId("htmlContent").setContent("<p>" + JSON.stringify(record).split(",").join("\n") + "</p>");
+			page.setModel(new JSONModel({
+				list: record.tags.split(" ").map(function (tag) {
+					return {
+						editable: tag !== record.type,
+						id: tag
+					};
+				})
+			}), "tags");
 			page.setSelectedSection(this.byId("content").getId());
 			page.setBusy(false);
 		},
