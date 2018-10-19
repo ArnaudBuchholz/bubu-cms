33f930e8ca7e67a81a345db02f70c474c0cddb6d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-19 15:39:26 -0400

    Put in common ODATA implementation

--- api/odata.js
+++ api/odata.js
@@ -3,6 +3,7 @@
 const
     gpf = global.gpf || require("gpf-js/source"),
     attributes = require("./attributes"),
+    db = require("./db"),
     searcher = require("./search"),
     sorter = require("./sort"),
 
@@ -14,39 +15,61 @@ const
         "string": (aggregated, key, value) => {
             aggregated[key] = value;
         }
-    }
-;
+    },
 
-module.exports = {
+    mapOfSerialTypeMatcher = {
+        "undefined": "",
+        "number": "([0-9]+)",
+        "string": "'([^']+)'"
+    },
 
-    buildToJSON: EntityClass => {
+    mapOfSerialTypeToJSON = {
+        "undefined": () => "",
+        "number": value => value,
+        "string": value => `'${value}'`
+    },
+
+    buildToJSON = EntityClass => {
         const
-            keys = gpf.attributes.get(EntityClass, attributes.Key),
-            serialProps = gpf.serial.get(EntityClass),
-            keyProperty = serialProps[Object.keys(keys)[0]].name;
+            keys = Object.keys(gpf.attributes.get(EntityClass, attributes.Key)),
+            serialProps = gpf.serial.get(EntityClass);
         return entity => {
-            const raw = gpf.serial.toRaw(entity, function (value, property) {
-                if (gpf.serial.types.datetime === property.type) {
-                    if (value) {
-                        return "/Date(" + value.getTime() + ")/";
-                    } else {
-                        return null;
+            const
+                raw = gpf.serial.toRaw(entity, function (value, property) {
+                    if (gpf.serial.types.datetime === property.type) {
+                        if (value) {
+                            return "/Date(" + value.getTime() + ")/";
+                        } else {
+                            return null;
+                        }
                     }
-                }
-                if ("tags" === property.name) {
-                    return value.join(" ");
-                }
-                return value;
-            });
+                    if ("tags" === property.name) {
+                        return value.join(" ");
+                    }
+                    return value;
+                }),
+                uriKey = keys
+                    .map(key => serialProps[key])
+                    .map(keyProperty => {
+                        return {
+                            name: keyProperty.name,
+                            value: mapOfSerialTypeToJSON[keyProperty.type](raw[keyProperty.name])
+                        }
+                    })
+                    .map(keyInfo => keys.length === 1
+                        ? keyInfo.value
+                        : `${keyInfo.name}=${keyInfo.value}`
+                    )
+                    .join(",");
             raw.__metadata = {
-                uri: `${EntityClass.name}Set('${raw[keyProperty]}')`,
+                uri: `${EntityClass.name}Set(${uriKey})`,
                 type: `BUBU_CMS.${EntityClass.name}`
             };
             return raw;
         };
     },
 
-    parse: req => Object.assign(req.url
+    parse = req => Object.assign(req.url
         .substr(req.url.indexOf("?") + 1)
         .split("&")
         .reduce((aggregated, param) => {
@@ -87,6 +110,91 @@ module.exports = {
 
                 }
             }
-        })
+        }),
+
+    prepare = EntityClass => {
+        EntityClass.toJSON = buildToJSON(EntityClass);
+        if (!EntityClass.byId) {
+            EntityClass.byId = () => Promise.resolve(null);
+        }
+        if (!EntityClass.all) {
+            EntityClass.all = () => Promise.resolve([]);
+        }
+    },
+
+    fail = (status, text, next) => {
+        var error = new Error(text);
+        error.status = status;
+        next(error);
+    },
+
+    notFound = next => fail(404, "Not found", next)
+;
+
+let
+    parserInstalled = false;
+
+module.exports = {
+
+    route: (router, EntityClass) => {
+
+        if (!parserInstalled) {
+            router.get("*", (req, res, next) => {
+                req.odata = parse(req);
+                next(); // pass control to the next handler
+            });
+            parserInstalled = true;
+        }
+
+        prepare(EntityClass);
+
+        const
+            serialProps = gpf.serial.get(EntityClass),
+            keys = Object.keys(gpf.attributes.get(EntityClass, attributes.Key));
+        let
+            keysMatcher;
+
+        if (keys.length === 1) {
+            keysMatcher = mapOfSerialTypeMatcher[serialProps[keys[0]].type];
+        } else {
+            keysMatcher = keys
+                .map(key => serialProps[key])
+                .map(keyProperty => `${keyProperty.name}=${mapOfSerialTypeMatcher[keyProperty.type]}`)
+                .join(",");
+        }
+
+        attributes.navigationProperties(EntityClass).forEach(property => {
+            router.get(new RegExp(`\/${EntityClass.name}Set\\(${keysMatcher}\\)\\/${property.getName()}`), (req, res, next) =>
+                db.open()
+                    .then(() => EntityClass.byId(req.params))
+                    .then(record => record
+                        ? record[property.getMemberName()]()
+                            .then(records => req.odata.send(property.to(), records, res))
+                            .catch(reason => fail(500, reason.toString, next))
+
+                        : notFound(next)
+                    )
+            );
+        });
+
+        router.get(new RegExp(`\/${EntityClass.name}Set\\(${keysMatcher}\\)`), (req, res, next) =>
+            db.open()
+                .then(() => EntityClass.byId(req.params))
+                .then(record => record
+                    ? req.odata.send(EntityClass, record, res)
+                    : notFound(next)
+                )
+        );
+
+        router.get(`/${EntityClass.name}Set`, (req, res, next) =>
+            db.open()
+                .then(() => EntityClass.searchable && req.odata.search
+                    ? searcher(EntityClass, req.odata.search)
+                    : EntityClass.all()
+                )
+                .then(records => req.odata.send(EntityClass, records, res))
+        );
+
+    }
 
 };
