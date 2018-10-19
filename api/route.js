938ccca5e60dc49463b30ac2446313d51bdfba46
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-19 15:39:39 -0400

    Remove ODATA specific stuff

--- api/route.js
+++ api/route.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const
     express = require("express"),
     path = require("path"),
@@ -6,22 +7,9 @@ const
     nanoid = require("nanoid"),
 
     config = require("../config"),
-    attributes = require("./attributes"),
     metadata = require("./metadata"),
-    entities = require("./entities"),
-    db = require("./db"),
-    odata = require("./odata"),
-    Record = require("./Record"),
-    Tag = require("./Tag"),
-
-    fail = (status, text, next) => {
-        var error = new Error(text);
-        error.status = status;
-        next(error);
-    },
-
-    notFound = next => fail(404, "Not found", next),
-    notImplemented = next => fail(500, "Not implemented", next);
+    odata = require("./odata")
+;
 
 router.get("/id", (req, res, next) => {
     res.set("Content-Type", "text/plain");
@@ -43,55 +31,16 @@ router.get("*", (req, res, next) => {
     "rss,heapTotal,heapUsed,external"
         .split(",")
         .forEach(type => res.set(`x-memory-${type.toLowerCase()}`, memory[type]));
-    [Record,Tag]
-        .forEach(Type => res.set(`x-count-${Type.name.toLowerCase()}`, Type.all().length));
-    // ODATA handler
-    req.odata = odata.parse(req);
     next(); // pass control to the next handler
 });
 
-entities.forEach(EntityClass => {
-    const
-        navigationProperties = attributes.navigationProperties(EntityClass);
-    EntityClass.toJSON = odata.buildToJSON(EntityClass);
+require("./entities").forEach(EntityClass => odata.route(router, EntityClass));
 
-    navigationProperties.forEach(property => {
-        router.get(new RegExp(`\/${EntityClass.name}Set\\('([^']+)'\\)\\/${property.getName()}`), (req, res, next) =>
-            db.open().then(() => {
-                const
-                    record = EntityClass.byId(req.params[0]);
-                if (!record) {
-                    return notFound(next);
-                }
-                record[property.getMemberName()]()
-                    .then(records => req.odata.send(property.to(), records, res))
-                    .catch(reason => fail(500, reason.toString, next));
-            }));
-    });
-
-    router.get(new RegExp(`\/${EntityClass.name}Set\\('([^']+)'\\)`), (req, res, next) =>
-        db.open().then(() => {
-            const
-                record = EntityClass.byId(req.params[0]);
-            if (!record) {
-                return notFound(next);
-            }
-            req.odata.send(EntityClass, record, res);
-        }));
-
-    router.get(`/${EntityClass.name}Set`, (req, res, next) =>
-        db.open().then(() => {
-            let
-                records;
-            if (EntityClass.searchable && req.params.search) {
-                records = searcher(EntityClass, params.search);
-            } else {
-                records = EntityClass.all();
-            }
-            req.odata.send(EntityClass, records, res);
-        }));
+// Default
+router.get("*", (req, res, next) => {
+    var error = new Error("Not implemented");
+    error.status = 500;
+    next(error);
 });
-
-router.get(/.*/, (req, res, next) => notImplemented(next));
 
 module.exports = router;
