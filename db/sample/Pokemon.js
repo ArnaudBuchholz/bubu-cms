a429653205616a070fa96ea0db9730f3c0583379
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-20 08:32:29 -0400

    Use local cache (will be slow to initialize)

--- db/sample/Pokemon.js
+++ db/sample/Pokemon.js
@@ -1,11 +1,27 @@
 "use strict";
 
+const
+    fs = require("fs"),
+    path = require("path"),
+    cachedPokemons = path.join(__dirname, "cache", "pokemons.json"),
+
+    asyncRead = fileName => new Promise((resolve, reject) => {
+        fs.readFile(fileName, (err, content) => err
+            ? reject(err)
+            : resolve(content)
+        )
+    })
+;
+
 module.exports = db => {
 
     const
         Pokemon = gpf.define({
             $class: "Pokemon",
             $extend: db.Record,
+
+            _statusState1: db.Record.STATE.show,
+            _statusState2: db.Record.STATE.show,
 
             constructor: function (raw) {
                 // const
@@ -20,18 +36,35 @@ module.exports = db => {
 
         });
 
-    return gpf.http.get("https://pokeapi.co/api/v2/pokemon/")
-        .then(response => JSON.parse(response.responseText))
-        .then(response => Promise.all(response.results
-            .slice(0, 150)
-            .map(pokedef => gpf.http.get(pokedef.url))
-        ))
-        .then(responses => responses.map(response => JSON.parse(response.responseText)))
+    return asyncRead(cachedPokemons)
+        .then(fileContent => JSON.parse(fileContent.toString()))
+        .catch(() => gpf.http.get("https://pokeapi.co/api/v2/pokemon/")
+            .then(response => JSON.parse(response.responseText))
+            .then(response => {
+                const pokemons = [];
+                return gpf.forEachAsync(response.results, (result, idx) => {
+                    console.log(`Downloading ${result.url}`);
+                    return gpf.http.get(result.url)
+                        .then(response => JSON.parse(response.responseText))
+                        .then(pokemon => {
+                            pokemons[idx] = pokemon;
+                        })
+                }).then(() => pokemons)
+            })
+            .then(pokemons => {
+                fs.writeFile(cachedPokemons, JSON.stringify(pokemons));
+                return pokemons;
+            })
+        )
         .then(pokemons => {
             return db.loadRecords(pokemons.map(data => new Pokemon({
                 id: `pokemon.${data.order}`,
                 icon: data.sprites.front_default,
-                name: data.name
+                name: data.name,
+                number: data.order,
+                statusText1: data.height,
+                statusText2: data.weight,
+                tags: data.types.map(type => type.type.name).join(" ")
             })));
         });
 
