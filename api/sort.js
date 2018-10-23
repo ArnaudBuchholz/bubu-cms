1e5503913c106fc04869cc45e6d0f110da918254
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:28 -0400

    Standard JS

--- api/sort.js
+++ api/sort.js
@@ -1,45 +1,48 @@
-"use strict";
+'use strict'
 
-const
-    gpf = global.gpf || require("gpf-js/source"),
-    sortersBySerialType = {};
+const gpf = global.gpf || require('gpf-js/source')
+const sortersBySerialType = {}
+const Func = Function
 
 sortersBySerialType[gpf.serial.types.string] = (name, ascending) =>
-    `if (a.${name} !== b.${name}) {
-        return ${ascending ? "a" : "b"}.${name}.localeCompare(${ascending ? "b" : "a"}.${name});
-    }`;
+  `if (a.${name} !== b.${name}) {
+        return ${ascending ? 'a' : 'b'}.${name}.localeCompare(${ascending ? 'b' : 'a'}.${name});
+    }`
 
 sortersBySerialType[gpf.serial.types.integer] = (name, ascending) =>
-    `if (a.${name} !== b.${name}) {
-        return ${ascending ? "a" : "b"}.${name} - ${ascending ? "b" : "a"}.${name};
-    }`;
+  `if (a.${name} !== b.${name}) {
+        return ${ascending ? 'a' : 'b'}.${name} - ${ascending ? 'b' : 'a'}.${name};
+    }`
 
 sortersBySerialType[gpf.serial.types.datetime] = (name, ascending) =>
-    `if (a.${name}.getTime() !== b.${name}.getTime()) {
-        return ${ascending ? "a" : "b"}.${name}.getTime() - ${ascending ? "b" : "a"}.${name}.getTime();
-    }`;
+  `if (a.${name}.getTime() !== b.${name}.getTime()) {
+        return ${ascending ? 'a' : 'b'}.${name}.getTime() - ${ascending ? 'b' : 'a'}.${name}.getTime();
+    }`
 
 module.exports = (EntityClass, orderBy) => {
-    const
-        serialProps = gpf.serial.get(EntityClass),
-        propsPerName = Object.keys(serialProps).reduce((dictionary, member) => {
-            const property = serialProps[member];
-            dictionary[property.name] = {
-                member: member,
-                type: property.type
-            };
-            return dictionary;
-        }, {}),
-        body = orderBy
-            .toLowerCase()
-            .split(",")
-            .map(criteria => {
-                const
-                    parts = (/(\w+) *(asc|desc)/).exec(criteria),
-                    property = propsPerName[parts[1]]
-                ;
-                return sortersBySerialType[property.type](property.member, parts[2] !== "desc")
-            });
-    body.push("return a._id.localeCompare(b._id);");
-    return new Function("a", "b", body.join("\n"));
+  const
+    serialProps = gpf.serial.get(EntityClass)
+
+  const propsPerName = Object.keys(serialProps).reduce((dictionary, member) => {
+    const property = serialProps[member]
+    dictionary[property.name] = {
+      member: member,
+      type: property.type
+    }
+    return dictionary
+  }, {})
+
+  const body = orderBy
+    .toLowerCase()
+    .split(',')
+    .map(criteria => {
+      const
+        parts = (/(\w+) *(asc|desc)/).exec(criteria)
+
+      const property = propsPerName[parts[1]]
+
+      return sortersBySerialType[property.type](property.member, parts[2] !== 'desc')
+    })
+  body.push('return a._id.localeCompare(b._id);')
+  return new Func('a', 'b', body.join('\n'))
 }
