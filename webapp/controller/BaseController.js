a4baa74de152b8f987747da4e599dbb9daa84719
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:59 -0400

    Standard JS

--- webapp/controller/BaseController.js
+++ webapp/controller/BaseController.js
@@ -1,72 +1,72 @@
+/* global sap */
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+  'sap/ui/core/mvc/Controller'
 
-], function(Controller) {
-	"use strict";
+], function (Controller) {
+  'use strict'
 
-	return Controller.extend("bubu-cms.controller.BaseController", {
+  return Controller.extend('bubu-cms.controller.BaseController', {
 
-		_getRouter: function () {
-			return this.getOwnerComponent().getRouter();
-		},
+    _getRouter: function () {
+      return this.getOwnerComponent().getRouter()
+    },
 
-		getTextIfInI18n: function (resourceBundle, key, params) {
-			if (resourceBundle.hasText(key)) {
-				return resourceBundle.getText(key, params);
-			}
-		},
+    getTextIfInI18n: function (resourceBundle, key, params) {
+      if (resourceBundle.hasText(key)) {
+        return resourceBundle.getText(key, params)
+      }
+    },
 
-		dbI18n: function (key, params) {
-			if (!this._dbI18n) {
-				this._dbI18n = this.getOwnerComponent().getModel("db.i18n").getResourceBundle();
-			}
-			return this.getTextIfInI18n(this._dbI18n, key, params);
-		},
+    dbI18n: function (key, params) {
+      if (!this._dbI18n) {
+        this._dbI18n = this.getOwnerComponent().getModel('db.i18n').getResourceBundle()
+      }
+      return this.getTextIfInI18n(this._dbI18n, key, params)
+    },
 
-		uiI18n: function (key, params) {
-			if (!this._uiI18n) {
-				this._uiI18n = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-			}
-			return this.getTextIfInI18n(this._uiI18n, key, params);
-		},
+    uiI18n: function (key, params) {
+      if (!this._uiI18n) {
+        this._uiI18n = this.getOwnerComponent().getModel('i18n').getResourceBundle()
+      }
+      return this.getTextIfInI18n(this._uiI18n, key, params)
+    },
 
-		i18n: function (type, key, parameters) {
-			var translationKey = type + "." + key,
-				result = this.dbI18n(translationKey, parameters);
-			if (undefined === result) {
-				result = this.uiI18n(translationKey, parameters);
-			}
-			return result;
-		},
+    i18n: function (type, key, parameters) {
+      var translationKey = type + '.' + key
 
-		formatIcon: function (type, icon) {
-			if (icon) {
-				return icon;
-			}
-			var defaultIcon = this.i18n(type, "defaultIcon");
-			if (defaultIcon) {
-				return "sap-icon://" + defaultIcon;
-			}
-			return "";
-		},
+      var result = this.dbI18n(translationKey, parameters)
+      if (undefined === result) {
+        result = this.uiI18n(translationKey, parameters)
+      }
+      return result
+    },
 
-		formatNumberUnit: function (type) {
-			return this.i18n(type, "numberUnit");
-		},
+    formatIcon: function (type, icon) {
+      if (icon) {
+        return icon
+      }
+      var defaultIcon = this.i18n(type, 'defaultIcon')
+      if (defaultIcon) {
+        return 'sap-icon://' + defaultIcon
+      }
+      return ''
+    },
 
-		formatStatus1: function (type) {
-			return this.i18n(type, "status1");
-		},
+    formatNumberUnit: function (type) {
+      return this.i18n(type, 'numberUnit')
+    },
 
-		formatStatus2: function (type) {
-			return this.i18n(type, "status2");
-		},
+    formatStatus1: function (type) {
+      return this.i18n(type, 'status1')
+    },
 
-		renderRating: function (value) {
-			return new Array(value + 1).join("\u2605")
-				+ new Array(6 - value).join("\u2606");
-		}
+    formatStatus2: function (type) {
+      return this.i18n(type, 'status2')
+    },
 
-	});
+    renderRating: function (value) {
+      return new Array(value + 1).join('\u2605') + new Array(6 - value).join('\u2606')
+    }
 
-});
+  })
+})
