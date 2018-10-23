36356bee0cab60272a7ddf67e823b8d2f5847050
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:50 -0400

    Standard JS

--- db/sample/init.js
+++ db/sample/init.js
@@ -1,51 +1,53 @@
-"use strict";
+'use strict'
 
-const
-    gpf = require("gpf-js/source"),
-    nanoid = require("nanoid"),
-    Record = require("../../api/Record"),
-    path = require("path"),
+const gpf = global.gpf || require('gpf-js/source')
+const nanoid = require('nanoid')
+const path = require('path')
 
-    _loadCSV = (db, name, RecordType) => {
-        const
-            csvFilePromise = gpf.fs.getFileStorage()
-                .openTextStream(path.join(__dirname, `${name}.csv`), gpf.fs.openFor.reading),
-            lineAdapter = new gpf.stream.LineAdapter(),
-            csvParser = new gpf.stream.csv.Parser(),
-            map = new gpf.stream.Map(function (raw) {
-                if (!raw.id) {
-                    raw.id = nanoid(); // as of now
-                }
-                return new RecordType(raw);
-            }),
-            output = new gpf.stream.WritableArray();
-        return csvFilePromise
-            .then(csvFile => gpf.stream.pipe(csvFile, lineAdapter, csvParser, map, output)
-                .then(function () {
-                    return db.loadRecords(output.toArray());
-                }));
-    },
+const _loadCSV = (db, name, RecordType) => {
+  const
+    csvFilePromise = gpf.fs.getFileStorage()
+      .openTextStream(path.join(__dirname, `${name}.csv`), gpf.fs.openFor.reading)
 
-    _fillDB = (db) => {
-        var Recipe = require("./Recipe")(db),
-            array = [];
-        for (var idx = 0; idx < 200000; ++idx) {
-            array.push(new Recipe({
-                id: nanoid(),
-                name: "My recipe number #" + (idx + 1),
-                calories: "" + Math.floor(Math.random() * 600),
-                portions: "" + Math.floor(Math.random() * 8),
-                rating: idx % 6
-            }));
-        }
-        return db.loadRecords(array);
+  const lineAdapter = new gpf.stream.LineAdapter()
+
+  const csvParser = new gpf.stream.csv.Parser()
+
+  const map = new gpf.stream.Map(function (raw) {
+    if (!raw.id) {
+      raw.id = nanoid() // as of now
     }
-;
+    return new RecordType(raw)
+  })
+
+  const output = new gpf.stream.WritableArray()
+  return csvFilePromise
+    .then(csvFile => gpf.stream.pipe(csvFile, lineAdapter, csvParser, map, output)
+      .then(function () {
+        return db.loadRecords(output.toArray())
+      }))
+}
+
+// const _fillDB = (db) => {
+//   var Recipe = require('./Recipe')(db)
+//
+//   var array = []
+//   for (var idx = 0; idx < 200000; ++idx) {
+//     array.push(new Recipe({
+//       id: nanoid(),
+//       name: 'My recipe number #' + (idx + 1),
+//       calories: '' + Math.floor(Math.random() * 600),
+//       portions: '' + Math.floor(Math.random() * 8),
+//       rating: idx % 6
+//     }))
+//   }
+//   return db.loadRecords(array)
+// }
 
 module.exports = db => Promise.all([
-    _loadCSV(db, "recipe", require("./Recipe")(db)),
-    _loadCSV(db, "contact", require("./Contact")(db)),
-    _loadCSV(db, "address", require("./Address")(db))
-    // require("./Pokemon")(db)
-    // _fillDB(db)
-]);
+  _loadCSV(db, 'recipe', require('./Recipe')(db)),
+  _loadCSV(db, 'contact', require('./Contact')(db)),
+  _loadCSV(db, 'address', require('./Address')(db))
+  // require("./Pokemon")(db)
+  // _fillDB(db)
+])
