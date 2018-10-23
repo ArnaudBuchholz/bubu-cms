ee895be1751ade95029c3f1502a0792a02559228
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:37 -0400

    Standard JS

--- db/sample/Address.js
+++ db/sample/Address.js
@@ -1,11 +1,13 @@
-"use strict";
+'use strict'
+
+const gpf = global.gpf || require('gpf-js/source')
 
 module.exports = db => gpf.define({
-    $class: "Address",
-    $extend: db.Record,
+  $class: 'Address',
+  $extend: db.Record,
 
-    constructor: function (raw) {
-        this.$super(raw);
-    }
+  constructor: function (raw) {
+    this.$super(raw)
+  }
 
-});
+})
