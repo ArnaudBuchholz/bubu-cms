3b2a8db4b628e300ad4172b872dd54766771d1b2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:44 -0400

    Standard JS

--- db/sample/Pokemon.js
+++ db/sample/Pokemon.js
@@ -1,71 +1,68 @@
-"use strict";
+'use strict'
 
-const
-    fs = require("fs"),
-    path = require("path"),
-    cachedPokemons = path.join(__dirname, "cache", "pokemons.json"),
+const gpf = global.gpf || require('gpf-js/source')
+const fs = require('fs')
+const path = require('path')
+const cachedPokemons = path.join(__dirname, 'cache', 'pokemons.json')
 
-    asyncRead = fileName => new Promise((resolve, reject) => {
-        fs.readFile(fileName, (err, content) => err
-            ? reject(err)
-            : resolve(content)
-        )
-    })
-;
+const asyncRead = fileName => new Promise((resolve, reject) => {
+  fs.readFile(fileName, (err, content) => err
+    ? reject(err)
+    : resolve(content)
+  )
+})
 
 module.exports = db => {
+  const
+    Pokemon = gpf.define({
+      $class: 'Pokemon',
+      $extend: db.Record,
 
-    const
-        Pokemon = gpf.define({
-            $class: "Pokemon",
-            $extend: db.Record,
+      _statusState1: db.Record.STATE.show,
+      _statusState2: db.Record.STATE.show,
 
-            _statusState1: db.Record.STATE.show,
-            _statusState2: db.Record.STATE.show,
+      constructor: function (raw) {
+        // const
+        //     calories = parseInt(raw.calories, 10),
+        //     portions = parseInt(raw.portions, 10);
+        // this._number = Math.floor(calories / portions);
+        // this._icon = `/images/recipe/${raw.id}.jpg`;
+        // this._statusText1 = portions.toString();
+        // this._statusText2 = raw.ready + "m";
+        this.$super(raw)
+      }
 
-            constructor: function (raw) {
-                // const
-                //     calories = parseInt(raw.calories, 10),
-                //     portions = parseInt(raw.portions, 10);
-                // this._number = Math.floor(calories / portions);
-                // this._icon = `/images/recipe/${raw.id}.jpg`;
-                // this._statusText1 = portions.toString();
-                // this._statusText2 = raw.ready + "m";
-                this.$super(raw);
-            }
+    })
 
-        });
-
-    return asyncRead(cachedPokemons)
-        .then(fileContent => JSON.parse(fileContent.toString()))
-        .catch(() => gpf.http.get("https://pokeapi.co/api/v2/pokemon/")
+  return asyncRead(cachedPokemons)
+    .then(fileContent => JSON.parse(fileContent.toString()))
+    .catch(() => gpf.http.get('https://pokeapi.co/api/v2/pokemon/')
+      .then(response => JSON.parse(response.responseText))
+      .then(response => {
+        const pokemons = []
+        return gpf.forEachAsync(response.results, (result, idx) => {
+          console.log(`Downloading ${result.url}`)
+          return gpf.http.get(result.url)
             .then(response => JSON.parse(response.responseText))
-            .then(response => {
-                const pokemons = [];
-                return gpf.forEachAsync(response.results, (result, idx) => {
-                    console.log(`Downloading ${result.url}`);
-                    return gpf.http.get(result.url)
-                        .then(response => JSON.parse(response.responseText))
-                        .then(pokemon => {
-                            pokemons[idx] = pokemon;
-                        })
-                }).then(() => pokemons)
+            .then(pokemon => {
+              pokemons[idx] = pokemon
             })
-            .then(pokemons => {
-                fs.writeFile(cachedPokemons, JSON.stringify(pokemons));
-                return pokemons;
-            })
-        )
-        .then(pokemons => {
-            return db.loadRecords(pokemons.map(data => new Pokemon({
-                id: `pokemon.${data.order}`,
-                icon: data.sprites.front_default,
-                name: data.name,
-                number: data.order,
-                statusText1: data.height,
-                statusText2: data.weight,
-                tags: data.types.map(type => type.type.name).join(" ")
-            })));
-        });
-
-};
+        }).then(() => pokemons)
+      })
+      .then(pokemons => {
+        fs.writeFile(cachedPokemons, JSON.stringify(pokemons))
+        return pokemons
+      })
+    )
+    .then(pokemons => {
+      return db.loadRecords(pokemons.map(data => new Pokemon({
+        id: `pokemon.${data.order}`,
+        icon: data.sprites.front_default,
+        name: data.name,
+        number: data.order,
+        statusText1: data.height,
+        statusText2: data.weight,
+        tags: data.types.map(type => type.type.name).join(' ')
+      })))
+    })
+}
