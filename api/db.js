04284065e8349fae31cf8b09ee614aa4335e536d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:12 -0400

    Standard JS

--- api/db.js
+++ api/db.js
@@ -1,26 +1,24 @@
-"use strict";
+'use strict'
 
-const
-    config = require("../config"),
-    Record = require("./Record");
+const config = require('../config')
+const Record = require('./Record')
 
-let
-    opened;
+let opened
 
 module.exports = {
 
-    open: () => {
-        if (!opened) {
-            try {
-                opened = require(`../db/${config.db}/init`)({
-                    Record: Record,
-                    loadRecords: array => Record.load(array)
-                });
-            } catch (e) {
-                console.error(e);
-            }
-        }
-        return opened;
+  open: () => {
+    if (!opened) {
+      try {
+        opened = require(`../db/${config.db}/init`)({
+          Record: Record,
+          loadRecords: array => Record.load(array)
+        })
+      } catch (e) {
+        console.error(e)
+      }
     }
+    return opened
+  }
 
-};
+}
