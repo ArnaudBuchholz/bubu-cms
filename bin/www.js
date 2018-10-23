8928584d688b14c7d152c2be6496baa9966090f0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:32 -0400

    Standard JS

--- bin/www.js
+++ bin/www.js
@@ -1,47 +1,44 @@
-"use strict";
+'use strict'
 
-const
-    app = require("../app"),
-    debug = require("debug")("foo:server"),
-    http = require("http"),
+const app = require('../app')
+const debug = require('debug')('foo:server')
+const http = require('http')
+const port = 3000
 
-    port = 3000;
+app.set('port', port)
 
-app.set("port", port);
+const server = http.createServer(app)
 
-const
-    server = http.createServer(app);
+server.listen(port)
 
-server.listen(port);
+server.on('error', error => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
 
-server.on("error", error => {
-    if (error.syscall !== "listen") {
-      throw error;
-    }
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
 
-    var bind = typeof port === "string"
-      ? "Pipe " + port
-      : "Port " + port;
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-      case "EACCES":
-        console.error(bind + " requires elevated privileges");
-        process.exit(1);
-        break;
-      case "EADDRINUSE":
-        console.error(bind + " is already in use");
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
-});
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
 
-server.on("listening", () => {
-    var addr = server.address();
-    var bind = typeof addr === "string"
-      ? "pipe " + addr
-      : "port " + addr.port;
-    debug("Listening on " + bind);
-});
+    default:
+      throw error
+  }
+})
+
+server.on('listening', () => {
+  var addr = server.address()
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port
+  debug('Listening on ' + bind)
+})
