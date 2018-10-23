f27401f5cc275a60d5b2a1c28c624c5b6c1890dc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:30 -0400

    Standard JS

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-"use strict";
-const
-    express = require("express"),
-    path = require("path"),
-    favicon = require("serve-favicon"),
-    logger = require("morgan"),
-    cookieParser = require("cookie-parser"),
-    bodyParser = require("body-parser"),
+'use strict'
 
-    config = require("./config"),
-    api = require("./api/route"),
-    openui5 = require("./routes/openui5"),
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const config = require('./config')
+const api = require('./api/route')
+const openui5 = require('./routes/openui5')
+const app = express()
 
-    app = express();
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use("/resources", openui5("resources"));
+app.use('/resources', openui5('resources'))
 // app.use("/test-resources", openui5("test-resources"));
-app.use(express.static(path.join(__dirname, "webapp")));
-app.use("/api", api);
-app.use("/images", express.static(path.join(__dirname, `db/${config.db}/images`)));
+app.use(express.static(path.join(__dirname, 'webapp')))
+app.use('/api', api)
+app.use('/images', express.static(path.join(__dirname, `db/${config.db}/images`)))
 
-app.disable("etag");
+app.disable('etag')
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    var err = new Error("Not Found");
-    err.status = 404;
-    next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use((err, req, res, next) => {
-    res.status(err.status || 500);
-    res.send(JSON.stringify(err));
-});
+  res.status(err.status || 500)
+  res.send(JSON.stringify(err))
+})
 
-module.exports = app;
+module.exports = app
