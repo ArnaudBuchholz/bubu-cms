d5ef2432467eeac8bb6652bafdce41ec048734d7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2018-10-22 23:49:42 -0400

    Standard JS

--- db/sample/Contact.js
+++ db/sample/Contact.js
@@ -1,21 +1,20 @@
-"use strict";
+'use strict'
 
-const
-    capitalize = str => str.charAt(0).toUpperCase() + str.substr(1)
-;
+const gpf = global.gpf || require('gpf-js/source')
+const capitalize = str => str.charAt(0).toUpperCase() + str.substr(1)
 
 module.exports = db => gpf.define({
-    $class: "Contact",
-    $extend: db.Record,
+  $class: 'Contact',
+  $extend: db.Record,
 
-    _statusState1: db.Record.STATE.show,
+  _statusState1: db.Record.STATE.show,
 
-    constructor: function (raw) {
-        this._icon = `/images/contact/${raw.id}.jpg`;
-        this._number = raw.phone;
-        this._name = `${capitalize(raw.firstname)} ${capitalize(raw.lastname)}`;
-        this._statusText1 = `${raw.firstname}.${raw.lastname}@starship-troopers.movie.com`;
-        this.$super(raw);
-    }
+  constructor: function (raw) {
+    this._icon = `/images/contact/${raw.id}.jpg`
+    this._number = raw.phone
+    this._name = `${capitalize(raw.firstname)} ${capitalize(raw.lastname)}`
+    this._statusText1 = `${raw.firstname}.${raw.lastname}@starship-troopers.movie.com`
+    this.$super(raw)
+  }
 
-});
+})
