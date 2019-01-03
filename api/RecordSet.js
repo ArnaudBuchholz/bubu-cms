cc69fed3c95a8c35d667c6ed799ff7f4034947d0
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-03 00:03:01 -0500

    Search implementation

--- api/RecordSet.js
+++ api/RecordSet.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const Set = require('./Set')
+const tagSet = require('./tagSet')
+const tagPrefix = '#'
+const tagPrefixLength = tagPrefix.length
 
 const records = []
 const recordsById = {}
@@ -20,8 +23,41 @@ class RecordSet extends Set {
     return Promise.resolve(recordsById[id])
   }
 
-  function query (sort, search) {
-    return Promise.resolve([]) // TO IMPLEMENT
+  function search (criteria) {
+    const
+      searchTerms = criteria
+        .split(' ')
+        .map(term => term.trim())
+        .filter(term => term)
+
+    const tags = searchTerms
+      .filter(term => term.indexOf(tagPrefix) === 0)
+      .map(term => tagSet.byId(term.substr(tagPrefixLength)))
+      .filter(tag => tag)
+      .sort((tag1, tag2) => tag1.count - tag2.count)
+    // Less references first
+
+    const terms = searchTerms
+      .filter(term => term.indexOf(tagPrefix) !== 0)
+
+    let searchResult
+    if (tags.length) {
+      // AND
+      searchResult = tags[0].records
+      tags.slice(1).forEach(tag => {
+        searchResult = searchResult.filter(record => record.hasTag(tag))
+      })
+    } else {
+      searchResult = records
+    }
+    if (terms.length) {
+      searchResult = searchResult.filter(record => terms.some(term => record.search(term))) // OR
+    }
+    return Promise.resolve(searchResult)
+  }
+
+  function sort (searchResult) {
+
   }
 
 }
