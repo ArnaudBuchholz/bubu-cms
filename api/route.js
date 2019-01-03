3a97b972e52fe38fd904bd5d86cbcc1c2609c092
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-02 23:21:58 -0500

    Isolates ODATA

--- api/route.js
+++ api/route.js
@@ -5,33 +5,31 @@ const path = require('path')
 const router = express.Router()
 const nanoid = require('nanoid')
 const config = require('../config')
-const metadata = require('./metadata')
-const odata = require('./odata')
+// const odata = require('./odata/route')
+const db = require('./db')
 
 router.get('/id', (req, res, next) => {
   res.set('Content-Type', 'text/plain')
   res.send(nanoid())
 })
 
-router.get(/\/\$metadata.*/, (req, res, next) => {
-  res.set('Content-Type', 'application/xml')
-  metadata().then(body => res.send(body))
-})
-
 router.get(/\/i18n(_\w+)?\.properties/, (req, res, next) => {
   res.sendFile(path.join(__dirname, '../db', config.db, req.url.substr(1)))
 })
 
-router.get('*', (req, res, next) => {
+router.all('*', (req, res, next) => {
   // Performance values
-  var memory = process.memoryUsage()
+  const memory = process.memoryUsage()
   'rss,heapTotal,heapUsed,external'
     .split(',')
     .forEach(type => res.set(`x-memory-${type.toLowerCase()}`, memory[type]))
+  // database selector
+  const urlDb = /(?:\?|&)db=([^&]+)(?:$|&)/.exec(req.url)[1]
+  req.db = db(urlDb || config.defaultDB)
   next() // pass control to the next handler
 })
 
-require('./entities').forEach(EntityClass => odata.route(router, EntityClass))
+// router.use('/odata', odata)
 
 // Default
 router.get('*', (req, res, next) => {
