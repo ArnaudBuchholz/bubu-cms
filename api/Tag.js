25fc97b8985cd04e7880d0c22891eb33d0eb3f7b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-02 23:22:50 -0500

    Tag

--- api/Tag.js
+++ api/Tag.js
@@ -1,75 +1,30 @@
 'use strict'
 
-let tag
+class Tag {
 
-const gpf = global.gpf || require('gpf-js/source')
-const Record = require('./Record')
-const MinDate = new Date(0)
-
-const Tag = gpf.define({
-  $class: 'Tag',
-  $extend: Record,
-
-  _type: 'tag',
-  _records: [],
-  _number: '0',
-
-  usedBy: function (record) {
-    this._records.push(record)
-    this._number = this._records.length.toString()
-  },
-
-  records: function (RecordType) {
-    if (RecordType) {
-      return this._records.filter(record => record instanceof RecordType)
-    } else {
-      return this._records
-    }
-  },
-
-  constructor: function (name) {
-    this._records = []
-    this._id = '#' + name
-    this._name = name
-    this._created = MinDate
-    this._modified = MinDate
-    Record.load([this])
-    if (tag) {
-      tag.usedBy(this)
-    }
-  },
-
-  toString: function () {
+  get name () {
     return this._name
   }
 
-})
-
-const tags = []
-
-const tagsByTag = {}
-
-Object.assign(Tag, {
-
-  all: () => tags,
-  get: tag => tagsByTag[tag.toLowerCase()],
-
-  allocate: tag => {
-    const loweredTag = tag.toLowerCase()
-    let tagRecord = tagsByTag[loweredTag]
-    if (!tagRecord) {
-      tagRecord = new Tag(loweredTag)
-      tags.push(tagRecord)
-      tagsByTag[loweredTag] = tagRecord
-    }
-    return tagRecord
+  get count () {
+    return this._records.length
   }
 
-})
+  get records () {
+    return this._records
+  }
 
-tag = Tag.allocate('tag')
+  add: function (record) {
+    this._records.push(record)
+  },
 
-// X-dependency
-Record.Tag = Tag
+  remove: function (record) {
+    this._records = this._records.filter(candidate => candidate !== record)
+  }
 
-module.exports = Tag
+  constructor (name) {
+    this._name = name
+    this._records = []
+  }
+
+}
