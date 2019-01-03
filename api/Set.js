663aacc0954e913d8d1d53bfa43aa9abb5aed3ad
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-02 23:22:17 -0500

    Entity Set definition

---
+++ api/Set.js
@@ -0,0 +1,19 @@
+'use strict'
+
+class Set {
+
+  function all () {
+    return Promise.resolve([])
+  }
+
+  function byId (id) {
+    return Promise.resolve()
+  }
+
+  function query (sort, search) {
+    return Promise.resolve([])
+  }
+
+}
+
+module.exports = Set
