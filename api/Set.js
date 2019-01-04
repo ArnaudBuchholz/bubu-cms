cde8cff4f3d6fa03598aa9cfd513408860350de6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-04 14:37:32 -0500

    standard JS

--- api/Set.js
+++ api/Set.js
@@ -1,27 +1,39 @@
 'use strict'
 
 class Set {
-
-  function all () {
+  all () {
     return Promise.resolve([])
   }
 
-  function byId (id) {
+  byId (id) {
     return Promise.resolve()
   }
 
-  function search (criteria) {
+  search (criteria) {
     return Promise.resolve([])
   }
 
-  function sort (searchResult) {
-    return searchResult
+  sort (searchResult, { field, ascending }) {
+    return searchResult.sort((item1, item2) => {
+      const value1 = item1[field] || ''
+      const typeofValue1 = typeof value1
+      const value2 = item2[field] || ''
+      let sign
+      if (ascending) {
+        sign = 1
+      } else {
+        sign = -1
+      }
+      if (typeof value1 === typeof value2 && (typeofValue1 instanceof Date || typeofValue1 === 'number')) {
+        return sign * (value1 - value2)
+      }
+      return sign * value1.toString().localeCompare(value2.toString())
+    })
   }
 
-  function query (search, sort) {
-    return this._search(search).then(this._sort.bind(this))
+  query (search = '', sort = { field: 'name', ascending: true }) {
+    return this._search(search).then(searchResult => this._sort(searchResult, sort))
   }
-
 }
 
 module.exports = Set
