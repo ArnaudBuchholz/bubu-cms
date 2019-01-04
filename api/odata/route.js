a829670cb4b9256d16a13f2cc8164cf26a68bb36
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-04 14:37:46 -0500

    standard JS

--- api/odata/route.js
+++ api/odata/route.js
@@ -2,8 +2,9 @@
 
 const express = require('express')
 const router = express.Router()
+const metadata = require('./metadata')
 
-require('./entities').forEach(EntityClass => odata.route(router, EntityClass))
+// require('./entities').forEach(EntityClass => odata.route(router, EntityClass))
 router.get(/\/\$metadata.*/, (req, res, next) => {
   res.set('Content-Type', 'application/xml')
   metadata().then(body => res.send(body))
