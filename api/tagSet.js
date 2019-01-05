0b30f8113c6d8fee0ce43b7137d8412c03cc2ac7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-05 08:36:03 -0500

    Renames and exposes as a class

--- api/tagSet.js
+++ api/tagSet.js
@@ -3,32 +3,35 @@
 const Set = require('./Set')
 const Tag = require('./Tag')
 
-const tags = []
-const tagsById = {}
-
 class TagSet extends Set {
   all () {
-    return Promise.resolve(tags)
+    return Promise.resolve(this._tags)
   }
 
   byId (id) {
-    return Promise.resolve(tagsById(id.toLowerCase()))
+    return Promise.resolve(this._tagsById[id.toLowerCase()])
   }
 
   search () {
-    return Promise.resolve(tags)
+    return Promise.resolve(this._tags)
   }
 
   allocate (tag) {
     const loweredTag = tag.toLowerCase()
-    let tagRecord = tagsById[loweredTag]
+    let tagRecord = this._tagsById[loweredTag]
     if (!tagRecord) {
       tagRecord = new Tag(loweredTag)
-      tags.push(tagRecord)
-      tagsById[loweredTag] = tagRecord
+      this._tags.push(tagRecord)
+      this._tagsById[loweredTag] = tagRecord
     }
     return tagRecord
   }
+
+  constructor () {
+    super()
+    this._tags = []
+    this._tagsById = {}
+  }
 }
 
-module.exports = new TagSet()
+module.exports = TagSet
