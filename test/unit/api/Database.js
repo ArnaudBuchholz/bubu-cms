9b609277a7ad76cdac87244ce3d62d56e670792c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-01-13 19:03:36 -0500

    Database loading tests

---
+++ test/unit/api/Database.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const assert = require('assert')
+const Database = require('../../../api/Database')
+
+describe('/api/Database.js', () => {
+  describe('loading', () => {
+    let db
+    beforeEach(() => {
+      db = new Database('sample')
+      return db.open()
+    })
+    it('loads record in the database', () => {
+      return db.records.all()
+        .then(records => {
+          assert.notStrictEqual(records.length, 0)
+        })
+    })
+  })
+})
