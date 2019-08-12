2565227f32f13b301531d07163ab15f0caed7e27
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-11 22:25:58 -0400

    Adapts to new API

--- api/odata/route.js
+++ api/odata/route.js
@@ -1,13 +1,6 @@
 'use strict'
 
-const express = require('express')
-const router = express.Router()
-const metadata = require('./metadata')
-
-// require('./entities').forEach(EntityClass => odata.route(router, EntityClass))
-router.get(/\/\$metadata.*/, (req, res, next) => {
-  res.set('Content-Type', 'application/xml')
-  metadata().then(body => res.send(body))
-})
-
-module.exports = router
+module.exports = [{
+  match: /^\/api\/odata\/\$metadata.*/,
+  custom: require('./metadata')
+}]
