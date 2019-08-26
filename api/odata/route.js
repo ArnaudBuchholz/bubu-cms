ff7cc46269b27bc9789a1f43e13ec4ae11b7b1ac
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-26 13:43:40 -0400

    New routing

--- api/odata/route.js
+++ api/odata/route.js
@@ -1,6 +1,26 @@
 'use strict'
 
-module.exports = [{
-  match: /^\/api\/odata\/\$metadata.*/,
-  custom: require('./metadata')
-}]
+require('url')
+
+const entities = require('./entities')
+const metadata = require('./metadata')
+
+const sets = {}
+
+entities.forEach(entity => {
+  const Set = entity.Set
+  if (Set) {
+    sets[set.name] = Set
+  }
+})
+
+module.exports = (request, response, relativeUrl) => {
+  if (relativeUrl.startsWith('$metadata')) {
+    return metadata(request, response)
+  }
+
+  const url = new URL(relativeUrl, 'http://localhost/')
+  console.log(request.method, url)
+
+  return 404
+}
