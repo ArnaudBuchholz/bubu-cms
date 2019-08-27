d17d8c68153ab2d03bd2d79293378186f081a0c5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-26 22:34:03 -0400

    Opens database

--- api/odata/route.js
+++ api/odata/route.js
@@ -4,20 +4,24 @@ require('url')
 
 const entities = require('./entities')
 const metadata = require('./metadata')
+const databases = require('../databases')
 
 const sets = {}
 
 entities.forEach(entity => {
   const Set = entity.Set
   if (Set) {
-    sets[set.name] = Set
+    sets[entity.name] = Set
   }
 })
 
-module.exports = (request, response, relativeUrl) => {
+module.exports = async (request, response, relativeUrl) => {
   if (relativeUrl.startsWith('$metadata')) {
     return metadata(request, response)
   }
+  const databaseName = request.headers.db || process.env.BUBU_CMS_DB_NAME || 'sample'
+  const database = databases(databaseName)
+  await database.open()
 
   const url = new URL(relativeUrl, 'http://localhost/')
   console.log(request.method, url)
