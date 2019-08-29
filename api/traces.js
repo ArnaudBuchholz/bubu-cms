69aa2038efce1106f25088d8a433529bf706965e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-29 13:26:30 -0400

    Documentation

--- api/traces.js
+++ api/traces.js
@@ -5,16 +5,16 @@ module.exports = {
   memory: (reference = {}) => {
     const usage = process.memoryUsage()
     console.log('MEMRY'.magenta, 'Memory report @'.gray, new Date().toISOString().magenta)
-      'rss,heapTotal,heapUsed,external'.split(',').forEach(name => {
-      const value = usage[name]
-      const last = reference[name]
+      'rss,heapTotal,heapUsed,external'.split(',').forEach(type => {
+      const value = usage[type]
+      const last = reference[type]
       let label
       if (last === undefined || last >= value) {
         label = value.toString().green
       } else {
         label = value.toString().red
       }
-      console.log('MEMRY'.magenta, `  ${name.padEnd(9, ' ')} (bytes) :`.gray, label)
+      console.log('MEMRY'.magenta, `  ${type.padEnd(9, ' ')} (bytes) :`.gray, label)
     })
     return usage
   }
