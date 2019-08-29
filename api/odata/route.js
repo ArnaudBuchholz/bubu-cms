2abb3dfafb25980a860e18568ab67f9faf6cef62
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-29 13:27:23 -0400

    Database is already opened

--- api/odata/route.js
+++ api/odata/route.js
@@ -5,7 +5,6 @@ require('url')
 const mime = require('mime')
 const entities = require('./entities')
 const metadata = require('./metadata')
-const databases = require('../databases')
 
 const jsonContentType = mime.getType('json')
 
@@ -13,6 +12,7 @@ async function getEntitySet (set, url, response) {
   response.writeHead(200, {
     'Content-Type': jsonContentType
   })
+  var all = await set.all()
   response.end(JSON.stringify({
     d: []
   }))
@@ -22,17 +22,14 @@ module.exports = async (request, response, relativeUrl) => {
   if (relativeUrl.startsWith('$metadata')) {
     return metadata(request, response)
   }
-  const databaseName = request.headers.db || process.env.BUBU_CMS_DB_NAME || 'sample'
-  const database = databases(databaseName)
-  await database.open()
 
   const url = new URL(relativeUrl, 'http://localhost/')
   if (request.method ==='GET') {
     if (url.pathname === '/RecordSet') {
-      return getEntitySet(database.records, url, response)
+      return getEntitySet(request.database.records, url, response)
     }
     if (url.pathname === '/TagSet') {
-      return getEntitySet(database.tags, url, response)
+      return getEntitySet(request.database.tags, url, response)
     }
   }
 
