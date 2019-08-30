90104e379466ce266ccb709cac3d2dc0bb7325af
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-08-30 14:10:43 -0400

    Compare unit test

---
+++ test/unit/api/compare.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const assert = require('assert')
+const compare = require('../../../api/compare')
+
+var now = new Date()
+
+describe('/api/compare.js', () => {
+  [
+    [undefined, undefined, 0],
+    [null, null, 0],
+    [true, true, 0],
+    [false, false, 0],
+    [1, 1, 0],
+    [0, 0, 0],
+    ["Hello", "Hello", 0],
+    ["", "", 0],
+    [now, now, 0],
+    [now, new Date(now.getTime()), 0],
+    ["123", "4", -1],
+    ["123", 4, -1],
+    ["123", "120", 1],
+    ["123", 120, 1],
+    [now, new Date(now.getTime() + 100), -100],
+    [now, new Date(now.getTime() - 100), 100],
+    [1, 100, -99],
+    [100, 1, 99]
+
+  ].forEach(([value1, value2, expected]) => {
+    it(`${JSON.stringify(value1)} <-> ${JSON.stringify(value2)} = ${expected}`, () => {
+      assert.strictEqual(compare(value1, value2), expected)
+    })
+  })
+})
