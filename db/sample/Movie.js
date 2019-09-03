0d73b046c966e5d21c3ceb9a29e088aac6d1159f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-09-02 23:37:31 -0400

    Build Movie record

--- db/sample/Movie.js
+++ db/sample/Movie.js
@@ -4,6 +4,17 @@ module.exports = db => {
     constructor (raw) {
       super()
       this._name = raw.title
+      this._id = this._buildId(raw.imdbid)
+      this._statusText1 = raw.year
+      this._statusText2 = `${raw.book} / ${raw.page}`
+      raw.genre.split(' ')
+        .forEach(genre => this.addTag(genre))
+      raw.actors.split(';')
+        .map(actor => actor.toLowerCase().replace(/ /g, '_'))
+        .forEach(actor => actor.includes('=')
+          ? this.addTag(actor.split('=')[0])
+          : this.addTag(actor)
+        )
     }
   }
 
