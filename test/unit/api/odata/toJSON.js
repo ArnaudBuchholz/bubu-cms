1ac880dc03523c8e8c32a1da693e497e576c3a15
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-09-03 21:58:16 -0400

    Also expose toJSON on Tag

--- test/unit/api/odata/toJSON.js
+++ test/unit/api/odata/toJSON.js
@@ -5,27 +5,41 @@ const Database = require('../../../../api/Database')
 require('../../../../api/odata/toJSON')
 
 describe('/api/odata/toJSON.js', () => {
-  describe('Record.toJSON()', () => {
-    let db
-    let MyRecord
-    let record
-    beforeEach(() => {
-      db = new Database('test')
-      MyRecord = class MyRecord extends db.Record {
-        constructor () {
-          super()
-          this._id = '1'
-          this._name = 'test'
-        }
+  let db
+  let MyRecord
+  let record
+  beforeEach(() => {
+    db = new Database('test')
+    MyRecord = class MyRecord extends db.Record {
+      constructor () {
+        super()
+        this._id = '1'
+        this._name = 'test'
       }
-      record = new MyRecord()
-    })
+    }
+    record = new MyRecord()
+  })
+  describe('Record.toJSON()', () => {
     it('exposes toJSON', () => assert(typeof record.toJSON === 'function'))
-    it('generates a valid ODATA json', () => {
-      // debugger;
-      const json = record.toJSON(); // _gpfDefineEntitiesFindByConstructor must be replace by import
-      assert.strictEqual(json.name,'test')
-      assert.strictEqual(json.__metadata.uri, 'MyRecordSet(\'1\')')
+    it('generates a valid ODATA json for record', () => {
+      const json = record.toJSON();
+      assert.strictEqual(json.name, 'test')
+      assert.strictEqual(json.__metadata.uri, `MyRecordSet('1')`)
+    })
+  })
+  describe('Tag.toJSON()', () => {
+    let tag
+    beforeEach(async () => {
+      const tags = await db.tags.all()
+      tag = tags[0]
+      assert(!!tag)
+    })
+    it('exposes toJSON', () => assert(typeof tag.toJSON === 'function'))
+    it('generates a valid ODATA json for tag', async () => {
+      const json = tag.toJSON();
+      assert.ok(!!json.name)
+      assert.notStrictEqual(json.count, 0)
+      assert.strictEqual(json.__metadata.uri, `TagSet('${json.name}')`)
     })
   })
 })
