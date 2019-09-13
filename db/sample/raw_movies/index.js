51fd6dc495fad70c1db8b6e9aa8f70a8c169ecfa
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-09-13 13:50:19 -0400

    Adds search buttons

--- db/sample/raw_movies/index.js
+++ db/sample/raw_movies/index.js
@@ -2,7 +2,9 @@
 
 const tags = {
   tr: 0,
-  td: 0
+  td: 0,
+  a: 0,
+  button: 0
 }
 
 Object.keys(tags).forEach(tag => {
@@ -16,16 +18,25 @@ gpf.http.get('raw_movies.csv')
     const lineAdapter = new gpf.stream.LineAdapter()
     const csvParser = new gpf.stream.csv.Parser()
     const output = new gpf.stream.WritableArray()
-    return gpf.stream.pipe(input, lineAdapter, csvParser, {
-      write: async data => console.log(data)
-    } /* output*/)
+    return gpf.stream.pipe(input, lineAdapter, csvParser, output)
       .then(() => output.toArray())
   })
   .then(movies => {
     const tbody = document.getElementById('movies')
     movies.forEach((movie, index) => {
       if (movie.title) {
-        tags.tr({ id: index }, tags.td(movie.title)).appendTo(tbody)
+        let imdbCell
+        if (movie.imdb) {
+            imdbCell = tags.td(tags.a({
+                target: '_blank',
+                href: `https://www.imdb.com/title/${movie.imdb}`
+            }, movie.imdb))
+        } else {
+            imdbCell = tags.button({
+                className: "btn btn-primary"
+            }, 'Search')
+        }
+        tags.tr({ id: index }, tags.td(movie.title), tags.td(imdbCell)).appendTo(tbody)
       }
     })
   })
