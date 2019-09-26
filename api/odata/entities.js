3ddc0c4c29b9f5a95474b07e57278ad570aa3c98
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-09-26 13:17:11 -0400

    Declares NavigationProperty

--- api/odata/entities.js
+++ api/odata/entities.js
@@ -1,7 +1,16 @@
 'use strict'
 
+const gpf = require('gpf-js')
+const attribute = gpf.attributes.decorator
+const NavigationProperty = require('./NavigationProperty')
+
+const Record = require('../Record')
+const Content = require('../Content')
+
+attribute(new NavigationProperty(Content, 1).on({ id: "recordId" }))(Record, 'content')
+
 module.exports = [
   require('../Tag'),
-  require('../Record'),
-  require('../Content')
+  Record,
+  Content
 ]
