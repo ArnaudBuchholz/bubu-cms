b9a74c1c90cf180ca769be1aa76a1a780275b61e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2019-09-28 22:21:03 -0400

    Adds ID

--- api/Content.js
+++ api/Content.js
@@ -2,6 +2,7 @@
 
 const gpf = require('gpf-js')
 const attribute = gpf.attributes.decorator
+const Id = require('./Id')
 
 class Content {
   constructor (recordId, data = '', mimeType = 'text/plain') {
@@ -11,8 +12,10 @@ class Content {
   }
 }
 
+attribute(new Id())(Content, '_recordId')
 attribute(new gpf.attributes.Serializable())(Content, '_recordId')
 attribute(new gpf.attributes.Serializable())(Content, '_data')
+attribute(new Id())(Content, '_mimeType')
 attribute(new gpf.attributes.Serializable())(Content, '_mimeType')
 
 module.exports = Content
