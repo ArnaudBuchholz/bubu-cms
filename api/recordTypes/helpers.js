b24dfa34161488ecd401c1f275d872c0f0111cb2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-02-23 23:11:47 -0500

    Helpers

---
+++ api/recordTypes/helpers.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  getAbsolutePath (db, fileName) {
+    if (path.isAbsolute(fileName)) {
+      return fileName
+    }
+    return path.join(db.path, fileName)
+  }
+
+  async addAllKeys (db, i18nFileName) {
+  }
+}
