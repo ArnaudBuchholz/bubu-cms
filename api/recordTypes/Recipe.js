673c857ec2c6d48bc4168f1acb80aa43990bb83e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-03 22:33:08 -0500

    Scanning for recipes

--- api/recordTypes/Recipe.js
+++ api/recordTypes/Recipe.js
@@ -15,60 +15,36 @@ module.exports = db => {
     const gpfFileStorage = gpf.fs.getFileStorage()
     const forReading = gpf.fs.openFor.reading
 
-    // Read movies
-    let count = -1
-    const csvFile = await gpfFileStorage.openTextStream(getAbsolutePath(db, csvFileName), forReading)
-    const lineAdapter = new gpf.stream.LineAdapter()
-    const csvParser = new gpf.stream.csv.Parser()
-    const factory = {
-      write: async function (csvRecord) {
-        ++count
-        const imdbId = imdb.select[count]
-        if (!imdbId) {
-          console.log('RECRD'.magenta, 'Movie'.blue, '400'.red, csvRecord.title.gray)
-          return // SKIP
-        }
-        const imdbMovie = imdb.movies[imdbId]
-        if (!imdbMovie) {
-          console.log('RECRD'.magenta, 'Movie'.blue, '404'.red, csvRecord.title.gray)
-          return // SKIP
-        }
-        const movie = new Movie()
-        movie._id = movie._buildId(`${csvFileName}#${count}`)
-        movie._imdbId = imdbId
-        movie._name = csvRecord.title
-        movie._number = `${csvRecord.book} / ${csvRecord.page}`
-        movie._statusText1 = imdbMovie.year
-        // 'Format' duration
-        const hours = Math.floor(imdbMovie.duration / 60)
-        const mins = imdbMovie.duration - 60 * hours
-        movie._statusText2 = `${hours.toString()}:${mins.toString().padStart(2, '0')}`
-        imdbMovie.genres.forEach(genre => movie.addTag(genre))
-        movie._cast = []
-        Object.keys(imdbMovie.cast).forEach(actorId => {
-          const tag = movie.addTag(actorId)
-          movie._cast.push({
-            id: tag.name,
-            role: imdbMovie.cast[actorId]
-          })
-        })
-        if (imdbMovie.image && imdbMovie.image.length) {
-          movie._image = imdbMovie.image[0]
-          movie._icon = imdbMovie.image[0].replace('.jpg', '._SX40_CR0,0,40,54_.jpg')
-        }
-        if (csvRecord.tags) {
-          csvRecord.tags.split(' ').forEach(tag => {
-            movie.addTag(tag)
-          })
-        }
-        console.log('RECRD'.magenta, 'Movie'.blue, '200'.green, movie.name.gray)
-      }
+    async function load (path, name) {
+      // const recipe = new Recipe()
+      // recipe._id = recipe._buildId(`${path}#${name}`)
+
+      console.log('RECRD'.magenta, 'Recipe'.blue, '200'.green, `${path}/${name}`.gray)
     }
-    await gpf.stream.pipe(csvFile, lineAdapter, csvParser, factory)
-      .catch(reason => {
-        console.log('RECRD'.magenta, 'Movie'.blue, '500'.red, reason.toString())
-      })
+
+    async function scan (path) {
+      const fileNames = await gpfFileStorage.explore(path)
+      const recipes = {}
+      const splitter = /^(.*)(\.\w+)?$/
+      while (fileNames.moveNext()) {
+        const fileName = fileNames.getCurrent()
+        console.log(fileName)
+        const parts = splitter.exec(fileName)
+        const name = parts[0]
+        const extension = parts[1]
+        if (['.md', '.json'].includes(extension)) {
+          if (!Object.prototype.hasOwnProperty.call(recipes, name)) {
+            recipes[name] = {}
+          }
+          recipes[name][extension] = true
+        }
+      }
+      return Promise.all(Object.keys(recipes).map(name => load(path, name, recipes[name])))
+    }
+
+    await scan(getAbsolutePath(db, folderName))
   }
 
-  return Movie
+
+  return Recipe
 }
