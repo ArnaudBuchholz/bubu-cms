0750c8abd34283aef37552d9d374c8759939f6db
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-03-03 22:33:15 -0500

    Adds recipes

--- db/sample/init.js
+++ db/sample/init.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = db => Promise.all([
-  db.recordTypes.Movie.load('movies.csv', 'movies.imdb.json')
-  // db.recordTypes.Recipe.load('recipe')
+  db.recordTypes.Movie.load('movies.csv', 'movies.imdb.json'),
+  db.recordTypes.Recipe.load('recipe')
 ])
