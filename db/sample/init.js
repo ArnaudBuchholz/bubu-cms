c7e1482cdb771cf870c8bee80813080815f274f6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-04-07 22:53:53 -0400

    Renames Recipes folder

--- db/sample/init.js
+++ db/sample/init.js
@@ -2,5 +2,5 @@
 
 module.exports = db => Promise.all([
   db.recordTypes.Movie.load('movies.csv', 'movies.imdb.json'),
-  db.recordTypes.Recipe.load('recipe')
+  db.recordTypes.Recipe.load('recipes')
 ])
