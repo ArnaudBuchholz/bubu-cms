182dcd0c00671ee786365f9b583027b1d30a6281
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-06 23:33:58 -0400

    Caching UI5 files (WIP)

--- index.js
+++ index.js
@@ -12,18 +12,39 @@ require('./reserve-src')
   }
 })
 
-const log = require('reserve/log')
-const path = require('path')
-const reserve = require('reserve/serve')
+const { createWriteStream, mkdir } = require('fs')
+const { dirname, join } = require('path')
+const { capture, log, serve } = require('reserve')
+
+const mkdirAsync = require('util').promisify(mkdir)
+
+const ui5version = /((?:open)ui5).*(\d+\.\d+\.\d+)$/.exec(process.env.BUBU_CMS_UI5_DIST)
+const ui5cache = `./cache/${ui5version[1]}/${ui5version[2]}`
 
 const configuration = {
   port: parseInt(process.env.BUBU_CMS_PORT, 10) || 3000,
   mappings: [{
-    match: /\/resources\/(.*)/,
-    url: `${process.env.BUBU_CMS_UI5_DIST}/resources/$1`
+    method: 'GET',
+    match: /\/((?:test-)?resources\/.*)/,
+    file: `${ui5cache}/$1`,
+    'ignore-if-not-found': true
   }, {
-    match: /\/test-resources\/(.*)/,
-    url: `${process.env.BUBU_CMS_UI5_DIST}/test-resources/$1`
+    method: 'GET',
+    match: /\/((?:test-)?resources\/.*)/,
+    custom: async (request, response, path) => {
+      const cachePath = join(ui5cache, path)
+      const cacheFolder = dirname(cachePath)
+      await mkdirAsync(cacheFolder, { recursive: true })
+      const file = createWriteStream(cachePath)
+      capture(response, file)
+        .catch(reason => {
+          console.error(`Unable to cache ${cachePath}`, reason)
+        })
+    }
+  }, {
+    method: 'GET',
+    match: /\/((?:test-)?resources\/.*)/,
+    url: `${process.env.BUBU_CMS_UI5_DIST}/$1`
   }, {
     match: /^\/api\/(.*)/,
     custom: require('./api/route')
@@ -31,12 +52,14 @@ const configuration = {
     match: /^\/api\/odata\/(.*)/,
     custom: require('./api/odata/route')
   }, {
+    method: 'GET',
     match: /^\/$/,
-    file: path.join(__dirname, 'webapp/index.html')
+    file: join(__dirname, 'webapp/index.html')
   }, {
+    method: 'GET',
     match: /(.*)/,
-    file: path.join(__dirname, 'webapp', '$1')
+    file: join(__dirname, 'webapp', '$1')
   }]
 }
 
-log(reserve(configuration))
+log(serve(configuration), process.argv.includes('--verbose'))
