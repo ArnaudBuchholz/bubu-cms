6bf42d0221bb671fb7ee5eb1a17fe208fc65029e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-06-08 23:18:43 -0400

    Moves reserve-src

--- core/reserve-src.js
+++ core/reserve-src.js
@@ -7,7 +7,5 @@ const fs = require('fs')
 if (process.env.BUBU_CMS_RESERVE_SRC) {
   const reserve = JSON.parse(fs.readFileSync('../reserve/package.json').toString())
   console.log(`REserve version ${reserve.version}`.gray)
-  'configuration,log,mock,serve'
-    .split(',')
-    .forEach(api => require('mock-require')(`reserve/${api}`, require(`../reserve/${api}`)))
+  require('mock-require')(`reserve`, require(`../reserve`))
 }
