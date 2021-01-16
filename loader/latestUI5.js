95c869b292942858f850425380a426ff27cbd53c
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-16 00:36:31 -0500

    Detect latest UI5

---
+++ loader/latestUI5.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const gpf = require('gpf-js')
+const openUI5cdn = process.env.OPENUI5_CDN || 'https://openui5.hana.ondemand.com'
+
+module.exports = async () => {
+  const neoAppUrl = openUI5 + '/neo-app.json'
+  const neoAppResponse = gpf.http.get(neoAppUrl)
+  if (neoAppResponse.statusCode !== 200) {
+    throw new Error(`Unable to GET ${neoAppUrl}`)
+  }
+  const neoApp = JSON.parse(neoAppResponse.responseText)
+  return openUI5cdn + neoApp.routes[0].path
+}
