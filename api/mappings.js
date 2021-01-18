22874f8441ebb0a83324e270f949c6fe13d97545
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-17 23:28:14 -0500

    Mappings factory

--- api/mappings.js
+++ api/mappings.js
@@ -2,18 +2,25 @@
 
 const { createWriteStream, mkdir } = require('fs')
 const mkdirAsync = require('util').promisify(mkdir)
-const { dirname, join } = require('path')
+const { basename, dirname, join } = require('path')
 const { capture } = require('reserve')
-const Database = rquire('./Database')
+const mime = require('mime')
 const Record = require('./Record')
 const Content = require('./Content')
 const Tag = require('./Tag')
 
+const textContentType = mime.getType('text')
+const xmlContentType = mime.getType('xml')
+
 module.exports = ({ ui5, port, db }) => {
   const ui5version = /((?:open)ui5).*(\d+\.\d+\.\d+)$/.exec(ui5)
   const ui5cache = join(__dirname, '../cache', ui5version[1], ui5version[2])
-  const database = new Database()
-
+  const fragments = (db.fragments || []).map(fragment => {
+    return {
+      match: new RegExp(basename(fragment).replace(/\./g, '\\.')),
+      file: fragment
+    }
+  })
   return {
     port,
     handlers: [{
@@ -41,6 +48,14 @@ module.exports = ({ ui5, port, db }) => {
       match: /\/((?:test-)?resources\/.*)/,
       url: `${ui5}/$1`
     }, {
+      custom: async (request, response) => {
+        request.db = db
+        const memory = process.memoryUsage()
+        'rss,heapTotal,heapUsed,external'
+          .split(',')
+          .forEach(type => response.setHeader(`x-memory-${type.toLowerCase()}`, memory[type]))
+      }
+    }, {
       match: /^\/api\/odata\/(.*)/,
       odata: '$1',
       'service-namespace': 'bubu-cms',
@@ -50,6 +65,21 @@ module.exports = ({ ui5, port, db }) => {
       match: /^\/$/,
       file: join(__dirname, '../webapp/index.html')
     }, {
+      match: /db_i18n(?:_(\w+))?\.properties/,
+      custom: async (request, response, lang) => {
+        const { db } = request
+        const i18n = db.getI18n(lang)
+        if (i18n) {
+          response.writeHead(200, {
+            'Content-Type': textContentType,
+            'Content-Length': i18n.length
+          })
+          response.end(i18n)
+        } else {
+          return 404
+        }
+      }
+    }, ...fragments, {
       match: /(.*)/,
       file: join(__dirname, '../webapp', '$1')
     }]
