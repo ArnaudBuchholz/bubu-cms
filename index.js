e42242300e708dad22b6064073db03aa22625fda
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-23 00:47:18 -0500

    Removes devMode

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const { log, serve } = require('reserve')
 const loader = require('./loader')
 const mappingsBuilder = require('./api/mappings')
 
-if (process.argv.includes('--dev')) {
-  require('./api/devMode')
-}
-
 const fileName = process.argv.filter(arg => !arg.startsWith('--'))[0] || 'index.js'
 loader(fileName)
   .then(settings => mappingsBuilder(settings))
