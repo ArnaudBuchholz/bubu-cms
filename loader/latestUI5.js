e85c12dca762067b6750c08adb9fcae78437b616
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-23 16:26:19 -0500

    Linter fix

--- loader/latestUI5.js
+++ loader/latestUI5.js
@@ -4,7 +4,7 @@ const gpf = require('gpf-js')
 const openUI5cdn = process.env.OPENUI5_CDN || 'https://openui5.hana.ondemand.com'
 
 module.exports = async () => {
-  const neoAppUrl = openUI5 + '/neo-app.json'
+  const neoAppUrl = openUI5cdn + '/neo-app.json'
   const neoAppResponse = gpf.http.get(neoAppUrl)
   if (neoAppResponse.statusCode !== 200) {
     throw new Error(`Unable to GET ${neoAppUrl}`)
