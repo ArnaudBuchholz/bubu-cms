e762147a212095c31aac3e2ff5a6269459f71a12
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-23 16:25:51 -0500

    Reading properties

--- api/entity/RecordType.js
+++ api/entity/RecordType.js
@@ -6,20 +6,23 @@ const Key = require('reserve-odata/attributes/Key')
 const NavigationProperty = require('reserve-odata/attributes/NavigationProperty')
 const TypeProperty = require('./TypeProperty')
 
-module.exports = class RecordType {
+class RecordType {
   get type () {
     return this._type
   }
 
   getProperties () {
-
+    return this._properties
   }
 
-  constructor (type) {
+  constructor (type, definition) {
     this._type = type
+    this._properties = TypeProperty.names.map(name => new TypeProperty(type, name, definition[name]))
   }
 }
 
 attribute(new gpf.attributes.Serializable())(RecordType, 'type')
 attribute(new Key())(RecordType, 'type')
 attribute(new NavigationProperty('properties', TypeProperty, '*'))(RecordType, 'getProperties')
+
+module.exports = RecordType
