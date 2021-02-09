19b84a15b7f9f53b0180f7332486c2060b177699
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-02-09 07:47:42 -0500

    Provide initializers

--- test/mocha/api/entity/Record.test.js
+++ test/mocha/api/entity/Record.test.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const assert = require('assert')
+const Database = require('../../../../api/Database')
+const RecordType = require('../../../../api/entity/RecordType')
 const Record = require('../../../../api/entity/Record')
 
 describe('/api/entity/Record', () => {
@@ -9,6 +11,20 @@ describe('/api/entity/Record', () => {
       const nullRecord = Object.create(Record.prototype)
       assert.strictEqual(nullRecord.hasOwnProperty('id'), false)
       assert.strictEqual(Record.prototype.hasOwnProperty('id'), true)
+    })
+  })
+
+  describe('loading', () => {
+    const db = new Database()
+    const rt = new RecordType('test', {
+      load: any => {}
+    })
+
+    it('fits known properties in private members', () => {
+      const record = new Record(db, rt, {
+        status1: 'status1'
+      })
+      assert.strictEqual(record._status1, 'status1')
     })
   })
 })
