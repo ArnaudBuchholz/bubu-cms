bab19b887ef43ddbae4b24b72e797d70ad54c717
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-11 00:55:22 -0400

    Linter fix + refs

--- src/types/IStorage.ts
+++ src/types/IStorage.ts
@@ -7,44 +7,44 @@ import {
   StoredRecord
 } from './StoredRecord'
 
-export type SearchOptions = {
+export interface SearchOptions {
   paging: {
-    skip: number,
+    skip: number
     top: number
-  },
+  }
   sort?: {
-    field: string,
+    field: string
     ascending: boolean
   }
-  refs: StoredRecordRefs,
+  refs: StoredRecordRefs
   search?: string
 }
 
-export type SearchResult = {
-  count: number,
-  records: StoredRecord[],
+export interface SearchResult {
+  count: number
+  records: StoredRecord[]
   refs: Record<StoredRecordType, Record<StoredRecordId, StoredRecord>>
 }
 
-export type UpdateInstructions = {
-  name?: string,
-  icon?: string,
-  rating?: StoredRecordRating,
-  touched?: Date,
-  tags: {
-    add: string[],
-    del: []
-  },
+export interface UpdateInstructions {
+  name?: string
+  icon?: string
+  rating?: StoredRecordRating
+  touched?: Date
+  refs: {
+    add: StoredRecordRefs
+    del: StoredRecordRefs
+  }
   fields: {
-    add: Fields,
+    add: Fields
     del: Fields
   }
 }
 
 export interface IStorage {
-  search (options: SearchOptions): Promise<SearchResult>
-  get (type: StoredRecordType, id: StoredRecordId): Promise<undefined | StoredRecord>
-  create (record: StoredRecord): Promise<void>
-  update (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions): Promise<void>
-  delete (type: StoredRecordType, id: StoredRecordId): Promise<void>
+  search: (options: SearchOptions) => Promise<SearchResult>
+  get: (type: StoredRecordType, id: StoredRecordId) => Promise<undefined | StoredRecord>
+  create: (record: StoredRecord) => Promise<void>
+  update: (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions) => Promise<void>
+  delete: (type: StoredRecordType, id: StoredRecordId) => Promise<void>
 }
