56f3daed4404781cce5d79376e86ddb044bf8504
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-11 00:55:30 -0400

    Linter fix

--- src/types/StoredRecord.ts
+++ src/types/StoredRecord.ts
@@ -1,16 +1,16 @@
-export type Fields = Record<string,undefined | string | number | Date>
+export type Fields = Record<string, undefined | string | number | Date>
 export type StoredRecordType = string
 export type StoredRecordId = string
 export type StoredRecordRating = 1 | 2 | 3 | 4 | 5
 export type StoredRecordRefs = Record<StoredRecordType, StoredRecordId[]>
 
-export type StoredRecord = {
-  type: StoredRecordType,
-  id: StoredRecordId,
-  name: string,
-  icon?: string,
-  rating?: StoredRecordRating,
-  touched?: Date,
-  refs: StoredRecordRefs,
+export interface StoredRecord {
+  type: StoredRecordType
+  id: StoredRecordId
+  name: string
+  icon?: string
+  rating?: StoredRecordRating
+  touched?: Date
+  refs: StoredRecordRefs
   fields: Fields
 }
