3ae34007c44c42fa5df4b195aeda6e0d175d8a76
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-17 00:46:19 -0400

    Testing search API

--- __tests__/api/search.test.ts
+++ __tests__/api/search.test.ts
@@ -1,12 +1,12 @@
-import { IStorage, SearchOptions, SearchResult } from '../../src/types/IStorage'
-import search from '../../src/api/search'
+import { StoredRecordType, StoredRecordId, /* StoredRecordRating, StoredRecordRefs, Fields, */ StoredRecord } from '../../src/types/StoredRecord'
+import { IStorage, SearchOptions, SearchResult, UpdateInstructions } from '../../src/types/IStorage'
+import { DEFAULT_PAGE_SIZE, search } from '../../src/api/search'
 
 type SearchResultAndOptions = SearchResult & {
   options: SearchOptions
 }
 
 describe('api/search', () => {
-  const $options = Symbol('options')
   const storage: IStorage = {
     async search (options: SearchOptions): Promise<SearchResult> {
       const result: SearchResultAndOptions = {
@@ -16,11 +16,48 @@ describe('api/search', () => {
         options
       }
       return result
-    }
+    },
+    async get (type: StoredRecordType, id: StoredRecordId): Promise<undefined | StoredRecord> { return undefined },
+    async create (record: StoredRecord): Promise<void> {},
+    async update (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions): Promise<void> {},
+    async delete (type: StoredRecordType, id: StoredRecordId): Promise<void> {}
   }
 
-  it('search all records', async () => {
-    const result: SearchResultAndOptions = await search(storage, '/records') as SearchResultAndOptions
-    // check result.options
+  describe('all records', () => {
+    it('search all records', async () => {
+      const result: SearchResultAndOptions = await search(storage, '/records') as SearchResultAndOptions
+      expect(result.options).toEqual({
+        paging: {
+          skip: 0,
+          top: DEFAULT_PAGE_SIZE
+        },
+        refs: {}
+      })
+    })
+
+    it('search all records with paging', async () => {
+      const result: SearchResultAndOptions = await search(storage, '/records?top=45&skip=20') as SearchResultAndOptions
+      expect(result.options).toEqual({
+        paging: {
+          skip: 20,
+          top: 45
+        },
+        refs: {}
+      })
+    })
+  })
+
+  describe('errors', () => {
+    it('validates parameters', async () => {
+      expect(await search(storage, '/records?unknown=abc&skip=20')).toThrow(Error)
+    })
+
+    it('validates skip & top parameters', async () => {
+      expect(await search(storage, '/records?top=abc&skip=20')).toThrow(Error)
+    })
+
+    it('validates syntax', async () => {
+      expect(await search(storage, '/anything')).toThrow(Error)
+    })
   })
 })
