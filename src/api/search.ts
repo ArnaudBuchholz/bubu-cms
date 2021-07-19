1d7ef0bdd2a478966287e8ba978d608c7cac581e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-18 23:46:04 -0400

    Adding more options

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,12 +1,12 @@
-import { IStorage, SearchResult } from '../types/IStorage'
+import { IStorage, SearchOptions, SearchResult, SortableField } from '../types/IStorage'
 
 export const DEFAULT_PAGE_SIZE: number = 50
 
-type urlParameterName = 'skip' | 'top'
+type urlParameterName = 'skip' | 'top' | 'sort' | 'search'
 type urlParameters = Record<urlParameterName, string>
 
 function isValidUrlParameterName (name: string): name is urlParameterName {
-  return ['skip', 'top'].includes(name)
+  return ['skip', 'top', 'sort', 'search'].includes(name)
 }
 
 function isANumber (value: string): void {
@@ -15,15 +15,29 @@ function isANumber (value: string): void {
   }
 }
 
+const sortRegexp: RegExp = /^(name|rating|touched)(?: (asc|desc))?$/
+function isASortField (value: string): void {
+  if (sortRegexp.exec(value) === null) {
+    throw new Error('Invalid request sort parameter')
+  }
+}
+
+function isAValidSearch (value: string): void {
+}
+
 const urlParameterValidators: Record<urlParameterName, (value: string) => void> = {
   skip: isANumber,
-  top: isANumber
+  top: isANumber,
+  sort: isASortField,
+  search: isAValidSearch
 }
 
 function extractUrlParameters (search: string): urlParameters {
   const parameters: urlParameters = {
     skip: '0',
-    top: DEFAULT_PAGE_SIZE.toString()
+    top: DEFAULT_PAGE_SIZE.toString(),
+    sort: '',
+    search: ''
   }
   if (search !== undefined) {
     search.replace(/&?(\w+)=([^&]+)/gy, (match: string, name: string, value: string): string => {
@@ -41,18 +55,32 @@ function extractUrlParameters (search: string): urlParameters {
 }
 
 export async function search (storage: IStorage, url: string): Promise<SearchResult> {
-  const [pathname, search] = url.split('?')
-  const { skip, top } = extractUrlParameters(search)
-  const paging = {
-    skip: parseInt(skip, 10),
-    top: parseInt(top, 10)
+  const [pathname, urlSearch] = url.split('?')
+  const { skip, top, sort, search } = extractUrlParameters(urlSearch)
+  const options: SearchOptions = {
+    paging: {
+      skip: parseInt(skip, 10),
+      top: parseInt(top, 10)
+    },
+    refs: {}
   }
-
-  if (pathname === '/records') {
-    return await storage.search({
-      paging,
-      refs: {}
-    })
+  const parsedSort = sortRegexp.exec(sort)
+  if (parsedSort !== null) {
+    options.sort = {
+      field: parsedSort[1] as SortableField,
+      ascending: parsedSort[2] !== 'desc'
+    }
   }
-  throw new Error('Invalid request')
+  if (search !== '') {
+    options.search = search
+  }
+  const parsedPathname = pathname.match(/\/records(?:\/(\$type|\$tag|\w+))?/)
+  if (parsedPathname === null) {
+    throw new Error('Invalid request')
+  }
+  const type: undefined | string = parsedPathname[1]
+  if (type !== undefined) {
+    options.refs.$type = [type]
+  }
+  return await storage.search(options)
 }
