189f6ab2953f8031fb8ae3a8cf1b3e5f7cab3ce7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-20 23:50:30 -0400

    Adds more type guard

--- src/types/StoredRecord.ts
+++ src/types/StoredRecord.ts
@@ -1,8 +1,12 @@
 export type FieldValue = undefined | string | number | Date
+export const MAX_FIELDVALUE_LENGTH = 256
 export function isFieldValue (value: any): value is FieldValue {
   const typeofValue: string = typeof value
-  if (['undefined', 'string'].includes(typeofValue)) {
+  if (typeofValue === 'undefined') {
     return true
+  }
+  if (typeofValue === 'string') {
+    return value.length <= MAX_FIELDVALUE_LENGTH
   }
   if (typeofValue === 'number') {
     return Math.round(value) === value // Integer
@@ -10,27 +14,38 @@ export function isFieldValue (value: any): value is FieldValue {
   return value instanceof Date
 }
 
-export type Fields = Record<string, FieldValue>
+export type FieldName = string
+export const MAX_FIELDNAME_LENGTH: number = 64
+function isValidName (value: any, maxLength: number): boolean {
+  return typeof value === 'string' && value.match(/^[a-zA-Z]+$/) !== null && value.length <= maxLength
+}
+export function isFieldName (value: any): value is FieldName {
+  return isValidName(value, MAX_FIELDNAME_LENGTH)
+}
+
+export type Fields = Record<FieldName, FieldValue>
+function isLiteralObject (value: any): value is Record<string, any> {
+  return value instanceof Object &&
+    Object.prototype.toString.call(value) === '[object Object]' &&
+    Object.getPrototypeOf(value) === Object.getPrototypeOf({})
+}
 export function isFields (value: any): value is Fields {
-  if (typeof value !== 'object') {
-    return false
-  }
-  return Object.keys(value).every((name: string) => isFieldValue(value[name]))
+  return isLiteralObject(value) &&
+    Object.keys(value).every((name: string) => isFieldName(name) && isFieldValue(value[name]))
 }
 
 export type StoredRecordType = string
+export const MAX_STOREDRECORDTYPE_LENGTH: number = 32
 export const $type: StoredRecordType = '$type'
 export const $tag: StoredRecordType = '$tag'
 export function isStoredRecordType (value: any): value is StoredRecordType {
-  if (typeof value !== 'string') {
-    return false
-  }
-  return [$type, $tag].includes(value) || value.match(/[a-zA-Z]+/) !== null
+  return [$type, $tag].includes(value) || isValidName(value, MAX_STOREDRECORDTYPE_LENGTH)
 }
 
 export type StoredRecordId = string
+export const MAX_STOREDRECORDID_LENGTH: number = 16
 export function IsStoredRecordId (value: any): value is StoredRecordId {
-  return typeof value === 'string'
+  return typeof value === 'string' && value.trim() === value && value.length > 0 && value.length <= MAX_STOREDRECORDID_LENGTH
 }
 
 export type StoredRecordRating = 1 | 2 | 3 | 4 | 5
