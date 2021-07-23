cc7e83c9d1b808b71ebc794c93738b1da8003b89
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-23 00:23:19 -0400

    Delete implementation

---
+++ src/api/delete.ts
@@ -0,0 +1,9 @@
+import { IStorage } from '../types/IStorage'
+import { StoredRecordId, StoredRecordType } from '../types/StoredRecord'
+
+export async function deleteRecord (storage: IStorage, type: StoredRecordType, id: StoredRecordId): Promise<void> {
+  if (await storage.get(type, id) === undefined) {
+    throw new Error('Not existing')
+  }
+  await storage.delete(type, id)
+}
