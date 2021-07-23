f0928d01170778c9d369c74987a77e05eb762ff7
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-23 00:23:13 -0400

    Testing delete

---
+++ __tests__/api/delete.test.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+import { StoredRecordType, StoredRecordId, StoredRecord } from '../../src/types/StoredRecord'
+import { IStorage, SearchOptions, SearchResult, UpdateInstructions } from '../../src/types/IStorage'
+import { deleteRecord } from '../../src/api/delete'
+
+describe('api/create', () => {
+  class Storage implements IStorage {
+    public deleted: undefined | StoredRecord = undefined
+    async search (options: SearchOptions): Promise<SearchResult> {
+      return { records: [], count: 0, refs: {} }
+    }
+
+    async get (type: StoredRecordType, id: StoredRecordId): Promise<undefined | StoredRecord> {
+      if (type === 'exists' && id === '123') {
+        return {
+          type: 'exists',
+          id: '123',
+          name: 'Already exists',
+          fields: {},
+          refs: {}
+        }
+      }
+      return undefined
+    }
+
+    async create (record: StoredRecord): Promise<void> {}
+    async update (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions): Promise<void> {}
+
+    async delete (type: StoredRecordType, id: StoredRecordId): Promise<void> {
+      this.deleted = await this.get(type, id)
+    }
+  }
+
+  const storage: Storage = new Storage()
+
+  it('ensures the received type and id identifies an existing record', async () => {
+    expect(async () => await deleteRecord(storage, 'unknown', '123')).rejects.toThrow(Error)
+    expect(storage.deleted).toEqual(undefined)
+  })
+
+  it('ensures the existing record is deleted', async () => {
+    await deleteRecord(storage, 'exists', '123')
+    expect(storage.deleted).not.toEqual(undefined)
+  })
+})
