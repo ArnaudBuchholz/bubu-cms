e22d7c759b15894db13cf458295904c39fa6f40f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-22 23:55:39 -0400

    Testing create

--- __tests__/api/create.test.ts
+++ __tests__/api/create.test.ts
@@ -10,10 +10,24 @@ describe('api/create', () => {
     async search (options: SearchOptions): Promise<SearchResult> {
       return { records: [], count: 0, refs: {} }
     }
-    async get (type: StoredRecordType, id: StoredRecordId): Promise<undefined | StoredRecord> { return undefined }
+
+    async get (type: StoredRecordType, id: StoredRecordId): Promise<undefined | StoredRecord> {
+      if (type === 'exists' && id === '123') {
+        return {
+          type: 'exists',
+          id: '123',
+          name: 'Already exists',
+          fields: {},
+          refs: {}
+        }
+      }
+      return undefined
+    }
+
     async create (record: StoredRecord): Promise<void> {
       this.created = record
     }
+
     async update (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions): Promise<void> {}
     async delete (type: StoredRecordType, id: StoredRecordId): Promise<void> {}
   }
@@ -21,7 +35,30 @@ describe('api/create', () => {
   const storage: Storage = new Storage()
 
   it('ensures the received body is a valid StoredRecord', async () => {
-    expect(() => create(storage, {
+    expect(async () => await create(storage, {
     })).rejects.toThrow(Error)
+    expect(storage.created).toEqual(undefined)
+  })
+
+  it('ensures an existing StoredRecord can not be recreated', async () => {
+    expect(async () => await create(storage, {
+      type: 'exists',
+      id: '123',
+      name: 'Attempt',
+      fields: {},
+      refs: {}
+    })).rejects.toThrow(Error)
+    expect(storage.created).toEqual(undefined)
+  })
+
+  it('creates new StoredRecord', async () => {
+    await create(storage, {
+      type: 'new',
+      id: '123',
+      name: 'Should work',
+      fields: {},
+      refs: {}
+    })
+    expect(storage.created).not.toEqual(undefined)
   })
 })
