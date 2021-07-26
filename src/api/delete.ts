9223a0a953ee930fb9b25cbb5bf116259f3ced65
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-07-25 23:31:01 -0400

    Use null instead of undefined

--- src/api/delete.ts
+++ src/api/delete.ts
@@ -2,7 +2,7 @@ import { IStorage } from '../types/IStorage'
 import { StoredRecordId, StoredRecordType } from '../types/StoredRecord'
 
 export async function deleteRecord (storage: IStorage, type: StoredRecordType, id: StoredRecordId): Promise<void> {
-  if (await storage.get(type, id) === undefined) {
+  if (await storage.get(type, id) === null) {
     throw new Error('Not existing')
   }
   await storage.delete(type, id)
