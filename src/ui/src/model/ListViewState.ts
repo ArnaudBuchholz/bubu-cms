27b3f1b616295a6cb154a704f237f775a64d1d71
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-13 16:18:18 -0400

    Attempt to control model

---
+++ src/ui/src/model/ListViewState.ts
@@ -0,0 +1,19 @@
+import JSONModel from 'sap/ui/model/json/JSONModel'
+
+export default class ListViewState extends JSONModel {
+  public search: undefined | string = ''
+  public sortingFieldLabel: undefined | string = 'name'
+  public sortingAscending: undefined | boolean = true
+}
+
+Object.getOwnPropertyNames(ListViewState.prototype).forEach(name => {
+  const defaultValue: any = (ListViewState.prototype as Record<string,any>)[name]
+  Object.defineProperty(ListViewState.prototype, name, {
+    get: function () : any {
+      return this.getProperty(`/${name}`) ?? defaultValue
+    },
+    set: function (value: any) {
+      this.setProperty(`/${name}`, value ?? defaultValue)
+    }
+  })
+})
