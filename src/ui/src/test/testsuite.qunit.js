8adb8e590f1f458bef8a53b16d2078c3ea0d242e
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-13 16:43:33 -0400

    Pattern for test suites

---
+++ src/ui/src/test/testsuite.qunit.js
@@ -0,0 +1,15 @@
+window.suite = function() {
+  'use strict'
+
+  const suite = new parent.jsUnitTestSuite()
+  const path = location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1)
+
+  suite.addTestPage(`${path}unit/unitTests.qunit.html`);
+
+  const xhr = new XMLHttpRequest()
+  xhr.open('GET', './integration/AllJourneys.json', false)
+  xhr.send(null)
+  JSON.parse(xhr.responseText).forEach(name => suite.addTestPage(`${path}integration/opaTestsComponent.qunit.html?journey=${name}`))
+
+  return suite
+};
