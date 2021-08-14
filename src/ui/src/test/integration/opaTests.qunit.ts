2580e686ef815e3e1dd1de0b40be3d1ccbb05d28
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-14 00:49:34 -0400

    Linter fix

--- src/ui/src/test/integration/opaTests.qunit.ts
+++ src/ui/src/test/integration/opaTests.qunit.ts
@@ -1,37 +1,35 @@
-QUnit.config.autostart = false;
+QUnit.config.autostart = false
 
 sap.ui.getCore().attachInit(async () => {
-	const pages = [
-		"Main"
+  const pages = [
+    'Main'
 
-	].map((name: string) => `beaver/poc/node/ui/test/integration/pages/${name}`);
+  ].map((name: string) => `beaver/poc/node/ui/test/integration/pages/${name}`)
 
-	const allJourneys = await fetch('AllJourneys.json');
-	const journeyNames: string[] = await allJourneys.json();
-	const [, selectedJourneyName] = /journey=([^&]*)/.exec(location.search) ?? [];
-	const journeys: string[] = journeyNames
-		.filter((name: string) => name === (selectedJourneyName ?? name))
-		.map((name: string) => `beaver/poc/node/ui/test/integration/${name}`);
+  const allJourneys = await fetch('AllJourneys.json')
+  const journeyNames: string[] = await allJourneys.json()
+  const [, selectedJourneyName] = /journey=([^&]*)/.exec(location.search) ?? []
+  const journeys: string[] = journeyNames
+    .filter((name: string) => name === (selectedJourneyName ?? name))
+    .map((name: string) => `beaver/poc/node/ui/test/integration/${name}`)
 
-	sap.ui.require([
-		"sap/ui/test/Opa5",
-		"Startup",
-		...pages,
-		...journeys
-	], (Opa5: any, Startup: any) => {
-		
-		Opa5.extendConfig({
-			arrangements : new Startup(),
-			viewNamespace : "bubu-cms.",
-			pollingInterval: 50,
-			autoWait: true,
-			appParams: {
-				"sap-ui-animation": false,
-				"sap-ui-language": "EN"
-			}
-		});
+  sap.ui.require([
+    'sap/ui/test/Opa5',
+    'Startup',
+    ...pages,
+    ...journeys
+  ], (Opa5: any, Startup: any) => {
+    Opa5.extendConfig({
+      arrangements: new Startup(),
+      viewNamespace: 'bubu-cms.',
+      pollingInterval: 50,
+      autoWait: true,
+      appParams: {
+        'sap-ui-animation': false,
+        'sap-ui-language': 'EN'
+      }
+    })
 
-		QUnit.start();
-	});
-
-});
+    QUnit.start()
+  })
+})
