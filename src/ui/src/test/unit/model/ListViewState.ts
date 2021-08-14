75314e60b75381a2c1cde38365b3bd18fb9c54e9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-14 00:49:49 -0400

    wip

--- src/ui/src/test/unit/model/ListViewState.ts
+++ src/ui/src/test/unit/model/ListViewState.ts
@@ -1,8 +1,8 @@
-import ListViewState from '../../../model/ListViewState'
+import ListViewState from 'bubu-cms/model/ListViewState'
 
 QUnit.module('model/ListViewState')
 
-QUnit.test("uses default values", function (assert: QUnit.Assert) {
+QUnit.test('uses default values', function (assert: QUnit.Assert) {
   const viewState = new ListViewState()
-  assert.strictEqual(viewState.name, 'name')
+  assert.strictEqual(viewState.sortingFieldLabel, 'name')
 })
