113a044f28f7548b8bd5ff0d34d8659838b87333
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-14 00:49:41 -0400

    Linter fix

--- src/ui/src/test/integration/StartAndStopJourney.ts
+++ src/ui/src/test/integration/StartAndStopJourney.ts
@@ -4,7 +4,7 @@ import { Opa5Given, Opa5When, Opa5Then } from './opa'
 QUnit.module('Start and Stop')
 
 opaTest('Starting and shutting down the app', function (Given: Opa5Given, When: Opa5When, Then: Opa5Then) {
-  Given.iStartMyApp();
+  Given.iStartMyApp()
   Then.onTheMainPage.iSeeThePage()
     .and.iTeardownMyApp()
 })
