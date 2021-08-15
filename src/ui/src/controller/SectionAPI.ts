8c53289092ea1e95070d2272caa6e4a872874fcd
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-15 01:27:38 -0400

    Section API

---
+++ src/ui/src/controller/SectionAPI.ts
@@ -0,0 +1,22 @@
+import BaseController from './BaseController'
+import URLHelper from 'sap/m/URLHelper'
+
+export default class SectionAPI {
+  private owner: BaseController
+
+  translateTag (tag: string): string {
+    return this.owner.i18n(`tag.${tag}`) || tag
+  }
+
+  navigateToListFilteredByTag (tag: string): void {
+    return this.owner.navigateToListFilteredByTag(tag)
+  }
+
+  redirect (url: string): void {
+    URLHelper.redirect(url)
+  }
+
+  constructor (controller: BaseController) {
+    this.owner = controller
+  }
+}
