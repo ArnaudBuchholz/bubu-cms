f69c4eaf11c41857db1a61702efb8f90c05ed06d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-17 07:47:09 -0400

    Linter fixes

--- src/ui/src/controller/SectionAPI.ts
+++ src/ui/src/controller/SectionAPI.ts
@@ -2,7 +2,7 @@ import BaseController from './BaseController'
 import URLHelper from 'sap/m/URLHelper'
 
 export default class SectionAPI {
-  private owner: BaseController
+  private readonly owner: BaseController
 
   translateTag (tag: string): string {
     return this.owner.i18n(`tag.${tag}`) || tag
