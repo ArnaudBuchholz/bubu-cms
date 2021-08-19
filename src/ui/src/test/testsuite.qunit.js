57632645f8119ae5aae14327d2ff7991ef50420d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-19 07:47:41 -0400

    Fixing errors

--- src/ui/src/test/testsuite.qunit.js
+++ src/ui/src/test/testsuite.qunit.js
@@ -1,12 +1,12 @@
 window.suite = function () {
   'use strict'
 
-  const suite = new parent.jsUnitTestSuite()
-  const path = location.pathname.substring(0, location.pathname.lastIndexOf('/') + 1)
+  const suite = new window.parent.jsUnitTestSuite() // eslint-disable-line new-cap
+  const path = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/') + 1)
 
   suite.addTestPage(`${path}unit/unitTests.qunit.html`)
 
-  const xhr = new XMLHttpRequest()
+  const xhr = new window.XMLHttpRequest()
   xhr.open('GET', './integration/AllJourneys.json', false)
   xhr.send(null)
   JSON.parse(xhr.responseText).forEach(name => suite.addTestPage(`${path}integration/opaTestsComponent.qunit.html?journey=${name}`))
