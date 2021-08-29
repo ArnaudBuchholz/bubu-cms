cd9b08896d228cd774b99d16d7610a9e7e6463c6
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-29 00:46:15 -0400

    Prepare loading

---
+++ src/loader/declareRecord.ts
@@ -0,0 +1,6 @@
+import { IStorage } from '../types/IStorage'
+import { StoredRecord } from '../types/StoredRecord'
+
+export async function declareRecord (storage: IStorage, record: StoredRecord): Promise<void> {
+
+}
