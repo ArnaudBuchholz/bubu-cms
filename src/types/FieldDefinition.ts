2bf923d1d9a318facae1b146335fc15ac67ad845
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-29 00:46:51 -0400

    Defines FieldType

--- src/types/FieldDefinition.ts
+++ src/types/FieldDefinition.ts
@@ -1,6 +1,8 @@
+export type FieldType = 'string' | 'number' | 'date'
+
 export interface FieldDefinition {
   name: string
-  type: 'string' | 'number' | 'date'
+  type: FieldType
   labelKey?: string
   regexp?: string
   placeholderKey?: string
