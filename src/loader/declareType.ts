ef290a578326fb47dc746df1db4377c4d655e043
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-29 00:46:20 -0400

    Prepare loading

---
+++ src/loader/declareType.ts
@@ -0,0 +1,21 @@
+import { IStorage } from '../types/IStorage'
+import { TypeDefinition } from '../types/TypeDefinition'
+import { StoredRecord, $type } from '../types/StoredRecord'
+
+async function createType (storage: IStorage, typeDefinition: TypeDefinition): Promise<void> {
+  
+}
+
+async function updateType (storage: IStorage, typeDefinition: TypeDefinition, existing: StoredRecord): Promise<void> {
+  throw new Error('Not implemented')
+}
+
+export async function declareType (storage: IStorage, typeDefinition: TypeDefinition): Promise<void> {
+  const { name } = typeDefinition
+  const existing: StoredRecord | null = await storage.get($type, name)
+  if (existing === null) {
+    await createType(storage, typeDefinition)
+  } else {
+    await updateType(storage, typeDefinition, existing)
+  }
+}
