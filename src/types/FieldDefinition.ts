50556c2d0658ca3e0e648cbf816be05663acbb19
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-08-31 07:25:11 -0400

    Merges FieldDefinition with TypeDefinition

--- src/types/FieldDefinition.ts
+++
@@ -1,9 +0,0 @@
-export type FieldType = 'string' | 'number' | 'date'
-
-export interface FieldDefinition {
-  name: string
-  type: FieldType
-  labelKey?: string
-  regexp?: string
-  placeholderKey?: string
-}
