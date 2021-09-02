06533b2af627cf9dd1507a14e38b4be95edc822a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-02 07:42:25 -0400

    Adjust API with new IStorage

--- src/api/create.ts
+++ src/api/create.ts
@@ -1,13 +1,9 @@
 import { IStorage } from '../types/IStorage'
-import { isStoredRecord } from '../types/StoredRecord'
+import { StoredRecordId, isStorableRecord } from '../types/StoredRecord'
 
-export async function create (storage: IStorage, jsonBody: object): Promise<void> {
-  if (!isStoredRecord(jsonBody)) {
+export async function create (storage: IStorage, jsonBody: object): Promise<StoredRecordId> {
+  if (!isStorableRecord(jsonBody)) {
     throw new Error('Not a record')
   }
-  const { type, id } = jsonBody
-  if (await storage.get(type, id) !== null) {
-    throw new Error('Already exists')
-  }
-  await storage.create(jsonBody)
+  return await storage.create(jsonBody)
 }
