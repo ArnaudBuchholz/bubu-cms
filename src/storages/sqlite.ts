751a23d3f6899997c83c5f05a72f5219743fd76a
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-02 07:42:48 -0400

    Adjust interface to new IStorage

--- src/storages/sqlite.ts
+++ src/storages/sqlite.ts
@@ -1,16 +1,23 @@
-import { StoredRecordId, StoredRecordType, StoredRecord } from '../types/StoredRecord'
+import { StoredRecordType, StoredRecordId, StorableRecord, StoredRecord } from '../types/StoredRecord'
 import { IStorage, SearchOptions, SearchResult, UpdateInstructions } from '../types/IStorage'
 
 export class SQLLiteStorage implements IStorage {
   // region IStorage
 
   async search (options: SearchOptions): Promise<SearchResult> {
+    return {
+      count: 0,
+      records: [],
+      refs: {}
+    }
   }
 
   async get (type: StoredRecordType, id: StoredRecordId): Promise<null | StoredRecord> {
+    return null
   }
 
-  async create (record: StoredRecord): Promise<void> {
+  async create (record: StorableRecord): Promise<StoredRecordId> {
+    return ''
   }
 
   async update (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions): Promise<void> {
