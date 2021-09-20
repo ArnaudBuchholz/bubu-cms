680b8ab6bb10e51abd5830900985b1b71d6eacfc
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-20 00:29:32 -0400

    wip

--- src/loader/index.ts
+++ src/loader/index.ts
@@ -1,11 +1,12 @@
 import { join } from 'path'
 import { readFile } from 'fs/promises'
 import { isConfiguration } from './types'
+import { storageFactory } from '../storages'
 
 export async function load (cwd: string): Promise<void> {
   const configuration: any = JSON.parse((await readFile(join(cwd, '.bubu-cms.json'))).toString())
   if (!isConfiguration(configuration)) {
     throw new Error('Invalid configuration')
   }
-  
+  const storage = storageFactory(configuration.storage)
 }
