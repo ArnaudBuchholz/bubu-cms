4ebc2e80d050dba2ccf7137798940564cbee4bd3
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-20 00:08:08 -0400

    Useless

--- src/loader/declareRecord.ts
+++
@@ -1,6 +0,0 @@
-import { IStorage } from '../types/IStorage'
-import { StoredRecord } from '../types/StoredRecord'
-
-export async function declareRecord (storage: IStorage, record: StoredRecord): Promise<void> {
-
-}
