a92e32451833b1e9420c3f80744dbd8e30fdf9ae
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-21 07:12:47 -0400

    Types def

--- src/loader/types.ts
+++ src/loader/types.ts
@@ -1,24 +1,50 @@
-import { isLiteralObject } from '../types/StoredRecord'
-import { TypeDefinition } from '../types/TypeDefinition'
+import { $tag, isLiteralObject } from '../types/StoredRecord'
+import { isTypeDefinition, isTypeName, TypeDefinition } from '../types/TypeDefinition'
 import { isStorageType, StorageType } from '../storages'
 
-export interface RecordSet {
+export interface CsvLoader {
   $type: string
   csv: string
+}
+export function isCsvLoader (value: any): value is CsvLoader {
+  if (!isLiteralObject(value)) {
+    return false
+  }
+  const { $type, csv } = value
+  return (isTypeName($type) || $type === $tag) &&
+    typeof csv === 'string'
+}
+
+export interface CustomLoader {
+  loader: string
+}
+export function isCustomLoader (value: any): value is CustomLoader {
+  if (!isLiteralObject(value)) {
+    return false
+  }
+  const { custom } = value
+  return typeof custom === 'string'
+}
+
+export type RecordLoader = CsvLoader | CustomLoader
+function isLoader (value: any): value is RecordLoader {
+  return isCsvLoader(value) || isCustomLoader(value)
 }
 
 export interface Configuration {
   serve?: number
   storage: StorageType
   types: TypeDefinition[]
-  records: RecordSet[]
+  loaders: RecordLoader[]
 }
 
 export function isConfiguration (value: any): value is Configuration {
   if (!isLiteralObject(value)) {
     return false
   }
-  const { serve, storage } = value
+  const { serve, storage, types, loaders } = value
   return (serve === undefined || typeof serve === 'number') &&
-    isStorageType(storage)
+    isStorageType(storage) &&
+    types.every((type: any) => isTypeDefinition(type)) &&
+    loaders.every((loader: any) => isLoader(loader))
 }
