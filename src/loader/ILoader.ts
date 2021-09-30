eb35ceaf964d7a1503eef429a7fbe638fcd9eb43
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-09-30 07:24:31 -0400

    Adapt interface to usage

--- src/loader/ILoader.ts
+++ src/loader/ILoader.ts
@@ -1,8 +1,9 @@
 import { StorableRecord, StoredRecordId } from '../types/StoredRecord'
-import { TypeName } from '../types/TypeDefinition'
+import { TypeName, TypeDefinition } from '../types/TypeDefinition'
 
 export interface ILoader {
   log: (...content: any[]) => void
-  getTypeId: (typeName: TypeName) => Promise<StoredRecordId | null>
+  getType: (typeName: TypeName) => Promise<TypeDefinition | null>
+  getTagId: (tagName: string) => Promise<StoredRecordId | null>
   create: (record: StorableRecord) => Promise<StoredRecordId>
 }
