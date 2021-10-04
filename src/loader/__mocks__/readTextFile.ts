6506a715a2e2ae25bf6b8a84652aa603d0e24ef5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-03 23:08:29 -0400

    Easier to mock in test file directly

--- src/loader/__mocks__/readTextFile.ts
+++
@@ -1,9 +0,0 @@
-const files: Record<string, string> = {}
-
-export async function readTextFile (path: string): Promise<string> {
-  return files[path]
-}
-
-readTextFile.set = (path: string, content: string): void => {
-  files[path] = content
-}
