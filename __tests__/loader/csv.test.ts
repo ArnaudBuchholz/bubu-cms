c9fcb4b13e8ca5ca3bf439b773b029c53ad999cf
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-09 00:00:23 -0400

    Improves coverage

--- __tests__/loader/csv.test.ts
+++ __tests__/loader/csv.test.ts
@@ -2,13 +2,18 @@ import { MemoryStorage } from '../../src/storages/memory'
 import { Loader } from '../../src/loader/Loader'
 import { saveTypeDefinition } from '../../src/types/TypeDefinition'
 import { loadFromCSV } from '../../src/loader/csv'
-import { StoredRecordType, $type } from '../../src/types/StoredRecord'
+import { StoredRecordType, $type, StoredRecordId, StorableRecord } from '../../src/types/StoredRecord'
+import { IStorage } from '../../src/types/IStorage'
 
 jest.mock('../../src/loader/readTextFile', () => {
   return {
     readTextFile: async function (path: string): Promise<string> {
-      if (path === '/test.csv') {
-        return `$name,string,number,date
+      if (path === '/full.csv') {
+        return `$name,string,number,date,$icon,$rating,$touched
+record 1,abc,123,2021-10-03T21:56:00,test.ico,4,2021-10-08T23:15:00`
+      }
+      if (path === '/wrong_column.csv') {
+        return `$name,string1,number,date
 record 1,abc,123,2021-10-03T21:56:00`
       }
       return ''
@@ -41,7 +46,7 @@ describe('csv/loader', () => {
     })
     await loadFromCSV(loader, {
       $type: 'record',
-      csv: '/test.csv'
+      csv: '/full.csv'
     })
   })
 
@@ -54,6 +59,18 @@ describe('csv/loader', () => {
     })
     expect(mockError).not.toBeCalled()
     expect(results.count).toBe(1)
+    const record = results.records[0]
+    expect(record).toMatchObject({
+      name: 'record 1',
+      icon: 'test.ico',
+      rating: 4,
+      touched: new Date('2021-10-08T23:15:00'),
+      fields: {
+        string: 'abc',
+        number: 123,
+        date: new Date('2021-10-03T21:56:00')
+      }
+    })
   })
 
   describe('error cases', () => {
@@ -64,7 +81,7 @@ describe('csv/loader', () => {
     it('fails on invalid type', async () => {
       await expect(loadFromCSV(loader, {
         $type: 'unknown',
-        csv: '/test.csv'
+        csv: '/full.csv'
       })).rejects.toMatchObject({
         message: 'Unknown type \'unknown\''
       })
@@ -76,6 +93,29 @@ describe('csv/loader', () => {
         csv: '/empty.csv'
       })).rejects.toMatchObject({
         message: 'Empty file'
+      })
+    })
+
+    it('fails on invalid column', async () => {
+      await expect(loadFromCSV(loader, {
+        $type: 'record',
+        csv: '/wrong_column.csv'
+      })).rejects.toMatchObject({
+        message: 'Unknown column for type \'record\''
+      })
+    })
+
+    it('fails upon creation failure', async () => {
+      const failingStorage: IStorage = Object.create(storage)
+      failingStorage.create = async function (record: StorableRecord): Promise<StoredRecordId> {
+        throw new Error('fail')
+      }
+      const failingLoader = new Loader(failingStorage)
+      await expect(loadFromCSV(failingLoader, {
+        $type: 'record',
+        csv: '/full.csv'
+      })).rejects.toMatchObject({
+        message: 'Error while storing record'
       })
     })
   })
