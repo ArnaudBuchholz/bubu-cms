72bd9ccf4cc21dc2181b766ea2e6d0d45cde205f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-16 10:09:52 -0400

    custom loader example

---
+++ __tests__/loader/check_loader.js
@@ -0,0 +1,21 @@
+'use strict'
+
+console.log('loading...')
+
+const assert = require('assert')
+
+export default async function (loader) {
+  assert.strictEqual(typeof loader, 'object')
+  assert.strictEqual(typeof loader.log, 'function')
+  assert.strictEqual(loader.log.length, 4)
+  loader.log('info', 'custom', 'Validated logger.log method')
+  loader.log('info', 'custom', 'Validating logger.getType method')
+  assert.strictEqual(typeof loader.getType, 'function')
+  assert.strictEqual(loader.getType.length, 1)
+  loader.log('info', 'custom', 'Getting record type')
+  const type = await loader.getType('record')
+  assert.notStrictEqual(type, null)
+  assert.strictEqual(typeof type.id, 'string')
+//   getTagId: (tagName: string) => Promise<StoredRecordId | null>
+//   create: (record: StorableRecord) => Promise<StoredRecordId>
+}
