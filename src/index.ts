ecc209a665e22dc0420d6124cdd285c642f2d768
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-10-30 00:31:00 -0400

    Load and serve

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 'use strict'
 
 import { load } from './loader'
+import { log, serve } from 'reserve'
 
-load(process.cwd()).catch(reason => console.error(reason))
+load(process.cwd())
+  .then(loader => {
+    const configuration = loader.buildReserveConfiguration()
+    log(serve(configuration))
+  })
+  .catch(reason => console.error(reason))
