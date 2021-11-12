140969788c5ae7f06265c2bc4774a89b61700c09
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-12 18:24:54 -0500

    Load types & pokemons

--- test/pokemons/pokemon.js
+++ test/pokemons/pokemon.js
@@ -12,10 +12,9 @@ async function csv (name) {
 module.exports = async loader => {
   loader.log('info', 'Loading pokemons...')
   loader.log('info', 'Creating pokemon types as tags...')
-  const tags = await csv('./types.csv')
   const types = {}
-  for await (const tag of tags) {
-    const [id, identifier] = tag.split(',')
+  for await (const record of await csv('./types.csv')) {
+    const [id, identifier] = record.split(',')
     const tagId = await loader.create({
       type: '$tag',
       name: identifier,
@@ -24,5 +23,34 @@ module.exports = async loader => {
     })
     loader.log('info', `${tagId} : ${identifier}`)
     types[id] = tagId
+  }
+  const pokemonTypes = {}
+  const tagTypeId = (await loader.getType('$tag')).id // Never assume it is $tag
+  loader.log('info', '', 'Loading pokemon types...', { tagTypeId })
+  for await (const record of await csv('./pokemon_types.csv')) {
+    const [id, typeId] = record.split(',')
+    if (pokemonTypes[id] === undefined) {
+      pokemonTypes[id] = []
+    }
+    pokemonTypes[id].push(types[typeId])
+  }
+  const pokemonTypeId = (await loader.getType('pokemon')).id
+  loader.log('info', '', 'Creating pokemons...', { pokemonTypeId })
+  for await (const record of await csv('./pokemon.csv')) {
+    const [pkid, identifier, speciesId, height, weight] = record.split(',')
+    const pokemonId = await loader.create({
+      type: pokemonTypeId,
+      name: identifier,
+      fields: {
+        pkid,
+        speciesId,
+        height,
+        weight
+      },
+      refs: {
+        [tagTypeId]: pokemonTypes[pkid]
+      }
+    })
+    loader.log('info', `${pokemonId} : ${identifier}`)
   }
 }
