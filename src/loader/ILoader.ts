ae572baf7fc1a7cbb75c7d505f5a1bbd214e35a9
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-14 10:15:13 -0500

    Expose search, update and delete

--- src/loader/ILoader.ts
+++ src/loader/ILoader.ts
@@ -1,4 +1,5 @@
-import { StorableRecord, StoredRecordId } from '../types/StoredRecord'
+import { SearchOptions, SearchResult } from '../types/IStorage'
+import { StorableRecord, StoredRecord, StoredRecordId } from '../types/StoredRecord'
 import { TypeName, StoredTypeDefinition } from '../types/TypeDefinition'
 
 export type LogType = 'info' | 'warning' | 'error' | 'fatal'
@@ -7,5 +8,9 @@ export interface ILoader {
   log: (type: LogType, module: string, message: string, details?: object) => void
   getType: (typeName: TypeName) => Promise<StoredTypeDefinition | null>
   getTagId: (tagName: string) => Promise<StoredRecordId | null>
+
+  search: (options: SearchOptions) => Promise<SearchResult>
   create: (record: StorableRecord) => Promise<StoredRecordId>
+  update: (record: StoredRecord) => Promise<void>
+  delete: (record: StoredRecord) => Promise<void>
 }
