d50dacd88068cd0ff648dd5eb83cf64d9bfbb0fb
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-22 00:47:37 -0500

    Storage model

--- src/ui/src/model/Storage.ts
+++ src/ui/src/model/Storage.ts
@@ -1,36 +1,21 @@
 import JSONModel from 'sap/ui/model/json/JSONModel'
-import { StoredRecordType, StoredRecordId, StorableRecord, StoredRecord } from '../types/StoredRecord'
-import { IStorage, SearchOptions, SearchResult, UpdateInstructions } from '../types/IStorage'
 
-export default class StorageModel extends JSONModel implements IStorage {
-  // region IStorage
+/**
+ * @namespace bubu-cms.model
+ */
+export default class Storage extends JSONModel {
+  private readonly pSequentialImportCompleted: Promise<void>
 
-  async search (options: SearchOptions): Promise<SearchResult> {
-    const response = await fetch('/api/search', {
-      method: 'GET'
-    })
-    const json = await response.json()
-    return json as SearchResult
+  private async init (): Promise<void> {
+    return await fetch('/api/allTypes')
+      .then(async response => await response.json())
+      .then(data => {
+        console.log(data)
+      })
   }
 
-  async get (type: StoredRecordType, id: StoredRecordId): Promise<null | StoredRecord> {
-    const response = await fetch('/api/get', {
-      method: 'GET'
-    })
-    const json = await response.json()
-    return json as StoredRecord
+  constructor () {
+    super()
+    this.pSequentialImportCompleted = this.init()
   }
-
-  async create (record: StorableRecord): Promise<StoredRecordId> {
-    return ''
-  }
-
-  async update (type: StoredRecordType, id: StoredRecordId, instructions: UpdateInstructions): Promise<void> {
-
-  }
-
-  async delete (type: StoredRecordType, id: StoredRecordId): Promise<void> {
-
-  }
-  // endregion
 }
