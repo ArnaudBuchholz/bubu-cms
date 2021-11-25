55a5ff7e167b2e1fbd6eced213a7d0aeadce9980
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-25 07:40:41 -0500

    IAsyncContentCreation

--- src/ui/src/Component.ts
+++ src/ui/src/Component.ts
@@ -7,14 +7,18 @@ import Log from 'sap/base/Log'
  */
 export default class Component extends UIComponent {
   public static metadata = {
-    manifest: 'json'
+    manifest: 'json',
+    interfaces: ['sap.ui.core.IASyncConentCreation']
   }
 
   public init (): void {
     super.init()
     const storage = this.getModel() as JSONModel
     storage.dataLoaded()
-      .then(() => this.getRouter().initialize())
+      .then(() => {
+        document.body.dataset.loading = 'off'
+        this.getRouter().initialize()
+      })
       .catch(reason => Log.error('Failed to initialize storage', reason.toString()))
   }
 }
