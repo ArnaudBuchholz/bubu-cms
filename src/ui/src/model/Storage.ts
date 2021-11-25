c96185992a8ab92b1870ded44937d9ab8ca34850
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-11-25 07:40:57 -0500

    Load types definition

--- src/ui/src/model/Storage.ts
+++ src/ui/src/model/Storage.ts
@@ -1,17 +1,20 @@
 import JSONModel from 'sap/ui/model/json/JSONModel'
+import { StoredRecordType } from '../types/StoredRecord'
+import { TypeDefinition } from '../types/TypeDefinition'
 
 /**
  * @namespace bubu-cms.model
  */
 export default class Storage extends JSONModel {
   private readonly pSequentialImportCompleted: Promise<void>
+  private readonly types: Record<StoredRecordType, TypeDefinition> = {}
 
   private async init (): Promise<void> {
     return await fetch('/api/allTypes')
       .then(async response => await response.json())
-      .then(data => {
-        console.log(data)
-      })
+      .then(data => data.forEach((typeDef: TypeDefinition) => {
+        this.types[typeDef.name] = typeDef
+      }))
   }
 
   constructor () {
