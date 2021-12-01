3fc80bf5126931f157197ba5d5e60cd6a346ba42
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-01 16:22:12 -0500

    Linter fix

--- src/ui/src/test/integration/opaPage.ts
+++ src/ui/src/test/integration/opaPage.ts
@@ -2,7 +2,7 @@ export function convert (PageMethods: Function): Record<string, () => {}> {
   const dictionary: Record<string, () => {}> = {}
   Object.getOwnPropertyNames(PageMethods.prototype)
     .filter(name => name !== 'constructor' && typeof PageMethods.prototype[name] === 'function')
-    .map(name => {
+    .forEach(name => {
       dictionary[name] = PageMethods.prototype[name]
     })
   return dictionary
