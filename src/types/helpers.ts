e1ff95f585b1e696d81bf333f9e180ddf29d0194
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-01 16:08:12 -0500

    Generic way to build type guard functions

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,3 +1,14 @@
 export function isThenable<T> (value: any): value is Promise<T> {
   return typeof value === 'object' && value !== null && typeof value.then === 'function'
 }
+
+export function isA<T> (checker: (value: any) => void): (value: any) => value is T {
+  return function (value: any): value is T {
+    try {
+      checker(value)
+      return true
+    } catch (e) {
+      return false
+    }
+  }
+}
