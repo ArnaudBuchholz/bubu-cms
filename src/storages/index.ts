a188f2a2437a7f41effa0749d82770cc6c5450ef
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-01 17:29:30 -0500

    checkStorageType

--- src/storages/index.ts
+++ src/storages/index.ts
@@ -1,11 +1,20 @@
 import { IStorage } from '../types/IStorage'
 import { MemoryStorage } from './memory'
 import { SQLiteStorage } from './sqlite'
+import { isA } from '../types/helpers'
 
 export type StorageType = 'memory' | 'sqlite'
-export function isStorageType (value: any): value is StorageType {
-  return ['memory', 'sqlite'].includes(value)
+
+export function checkStorageType (value: any): void {
+  if (typeof value !== 'string') {
+    throw new Error('Invalid storage type : expected string')
+  }
+  if (!['memory', 'sqlite'].includes(value)) {
+    throw new Error('Invalid storage type : unexpected value')
+  }
 }
+
+export const isStorageType = isA<StorageType>(checkStorageType)
 
 export function storageFactory (type: StorageType): IStorage | null {
   if (type === 'memory') {
