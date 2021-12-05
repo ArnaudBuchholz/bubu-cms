d3b7e251b35525dd8ae82031c3cd5a0eb75fd3d5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-12-05 00:05:55 -0500

    Temporary solution until decorators are working

--- src/ui/src/model/ListViewState.ts
+++ src/ui/src/model/ListViewState.ts
@@ -1,16 +1,22 @@
 import JSONModel from 'sap/ui/model/json/JSONModel'
 
-/**
- * @namespace bubu-cms.controller
- */
+/** @namespace bubu-cms.model */
 export default class ListViewState extends JSONModel {
+  public selectedType: undefined | string = ''
   public search: undefined | string = ''
   public sortingFieldLabel: undefined | string = 'name'
   public sortingAscending: undefined | boolean = true
 }
 
-Object.getOwnPropertyNames(ListViewState.prototype).forEach(name => {
-  const defaultValue: any = (ListViewState.prototype as Record<string, any>)[name]
+const mappings: any = {
+  selectedType: '',
+  search: '',
+  sortingFieldLabel: 'name',
+  sortingAscending: true
+}
+
+Object.keys(mappings).forEach(name => {
+  const defaultValue: any = mappings[name]
   Object.defineProperty(ListViewState.prototype, name, {
     get: function (): any {
       return this.getProperty(`/${name}`) ?? defaultValue
