1ff65ebbaa8830fe443288acf1b55a261a6481ae
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-04-21 07:34:29 -0400

    New proposal for ILoader

--- src/loader/ILoader.ts
+++ src/loader/ILoader.ts
@@ -1,13 +1,27 @@
-import { SearchOptions, SearchResult } from '../types/IStorage'
-import { StorableRecord, StoredRecord, StoredRecordId } from '../types/StoredRecord'
-import { TypeName, StoredTypeDefinition } from '../types/TypeDefinition'
+import { IStorage, SearchOptions, SearchResult } from '../types/IStorage'
+import { StorableRecord, StoredRecord, StoredRecordId, StoredRecordType } from '../types/StoredRecord'
+import { TypeName, StoredTypeDefinition, FieldType } from '../types/TypeDefinition'
 
 export type LogType = 'info' | 'warning' | 'error' | 'fatal'
 
+export interface ILoaderTypeFieldOptions {
+  type?: FieldType
+}
+
+export interface ILoaderType {
+  selectOrder: (position: number) => ILoaderType
+  defaultIcon: (value: string | ((record: StoredRecord) => string)) => ILoaderType
+  field: (name: string, options: ILoaderTypeFieldOptions) => ILoaderType
+  upsert: () => Promise<StoredRecordType>
+}
+
 export interface ILoader {
   log: (type: LogType, module: string, message: string, details?: object) => void
-  getType: (typeName: TypeName) => Promise<StoredTypeDefinition | null>
-  getTagId: (tagName: string) => Promise<StoredRecordId | null>
+
+  port: (serverPort: number) => void
+  storage: (storageType: string) => Promise<IStorage>
+
+  type: (name: string) => ILoaderType
 
   search: (options: SearchOptions) => Promise<SearchResult>
   create: (record: StorableRecord) => Promise<StoredRecordId>
