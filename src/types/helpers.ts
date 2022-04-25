2c7cd54a963b30bb7583e40644916a6f769e4641
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-04-25 00:25:56 -0400

    Improve type checking

--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,3 +1,20 @@
+export class NotATypeError extends Error {
+  constructor (
+    private readonly _type: string,
+    private readonly _reason: NotATypeError | Error | null = null
+  ) {
+    super(`Invalid ${_type}`)
+  }
+
+  get reason (): NotATypeError | Error | null {
+    return this._reason
+  }
+}
+
+export function notA (type: string, reason: NotATypeError | Error | null = null): never {
+  throw new NotATypeError(type, reason)
+}
+
 export function isA<T> (checker: (value: any) => asserts value is T): (value: any) => value is T {
   return function (value: any): value is T {
     try {
@@ -13,22 +30,19 @@ export function checkDate (value: any): asserts value is Date {
   if (typeof value !== 'object' ||
     value === null ||
     Object.prototype.toString.call(value) !== '[object Date]') {
-    throw new Error('Expected date')
+    notA('Date')
   }
 }
-
 export const isDate = isA(checkDate)
 
 export type LiteralObject = Record<string, any>
-
 export function checkLiteralObject (value: any): asserts value is LiteralObject {
   if (typeof value !== 'object' ||
     Object.prototype.toString.call(value) !== '[object Object]' ||
     Object.getPrototypeOf(value) !== Object.getPrototypeOf({})) {
-    throw new Error('Expected literal object')
+    notA('LiteralObject')
   }
 }
-
 export const isLiteralObject = isA(checkLiteralObject)
 
 export function isThenable<T> (value: any): value is Promise<T> {
