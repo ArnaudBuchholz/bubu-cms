fe3307ee669a454d8fffe51170173bce5795a409
ArnaudBuchholz <arnaud.buchholz@free.fr>
2022-04-30 23:54:40 -0400

    Adds test function for check and is

--- src/testTypeGuard.test.ts
+++ src/testTypeGuard.test.ts
@@ -1,6 +1,24 @@
-export default function (name: string, func: (value: any) => boolean, okValues: any[], koValues: any[]): void {
+export function testIsA (name: string, func: (value: any) => boolean, okValues: any[], koValues: any[]): void {
   describe(name, () => {
     okValues.forEach((value: any) => it(`accepts ${JSON.stringify(value)}`, () => expect(func(value)).toStrictEqual(true)))
     koValues.forEach((value: any) => it(`rejects ${JSON.stringify(value)}`, () => expect(func(value)).toStrictEqual(false)))
+  })
+}
+
+interface TypeIsAndCheck {
+  is: (value: any) => boolean
+  check: (value: any) => void
+}
+
+export function testType (name: string, methods: TypeIsAndCheck, okValues: any[], koValues: any[]): void {
+  describe(name, () => {
+    okValues.forEach((value: any) => {
+      it(`check accepts ${JSON.stringify(value)}`, () => expect(() => methods.check(value)).not.toThrowError())
+      it(`is accepts ${JSON.stringify(value)}`, () => expect(methods.is(value)).toStrictEqual(true))
+    })
+    koValues.forEach((value: any) => {
+      it(`check rejects ${JSON.stringify(value)}`, () => expect(() => methods.check(value)).toThrowError())
+      it(`is rejects ${JSON.stringify(value)}`, () => expect(methods.is(value)).toStrictEqual(false))
+    })
   })
 }
